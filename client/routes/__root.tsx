5290c2f80e3a057821f9fdb380e07d37aee01df1
Ethan Niser <ethan@niser.com>
2024-09-16 00:19:42 -0400

    show sharing code

--- client/routes/__root.tsx
+++ client/routes/__root.tsx
@@ -1,16 +1,18 @@
+import { FOO } from "@shared/index";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
+      <div className="flex gap-2 p-2">
         <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>{" "}
         <Link to="/about" className="[&.active]:font-bold">
           About
         </Link>
+        <div>{FOO}</div>
       </div>
       <hr />
       <Outlet />
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,8 @@
+import { FOO } from "@shared/index";
 import { eventHandler, toWebRequest } from "vinxi/http";
 
 // https://h3.unjs.io/guide/event-handler
 export default eventHandler(async (event) => {
   const _req = toWebRequest(event);
-  return new Response("Hello World!");
+  return new Response("Hello World!" + FOO);
 });
