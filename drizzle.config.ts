f02912cfa0a6cf9d7d4d47aa9159e7c16ea96009
Ethan Niser <ethan@niser.com>
2024-09-16 00:56:48 -0400

    drizzle?

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+import { SERVER_ENV } from "./shared/env";
+
+export default defineConfig({
+  schema: "./server/db/schema.ts",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: SERVER_ENV.DATABASE_URL,
+    authToken: SERVER_ENV.DATABASE_AUTH_TOKEN,
+  },
+});
