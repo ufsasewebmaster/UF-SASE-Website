463e9397c4ca492cfc74d5839aa6d79d782eeff0
Ethan Niser <ethan@niser.com>
2024-09-17 10:01:58 -0400

    fix drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./server/db/schema.ts",
   dialect: "sqlite",
+  driver: "turso",
   dbCredentials: {
     url: SERVER_ENV.DATABASE_URL,
     authToken: SERVER_ENV.DATABASE_AUTH_TOKEN,
