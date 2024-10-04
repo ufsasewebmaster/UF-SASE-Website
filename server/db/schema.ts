c4b9ccc0a06b404cc87d2d12b67d88439ed4dd56
ssihala <ssihala@tutanota.com>
2024-10-04 17:11:06 -0400

    Create user info table schemas

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -7,3 +7,47 @@ export const todos = sqliteTable("todo", {
   title: text("title").notNull(),
   completed: integer("completed", { mode: "boolean" }).notNull(),
 });
+
+export const users = sqliteTable("users", {
+  userID: text("userID").notNull().primaryKey(),
+  username: text("username").notNull(),
+  passwordHash: text("passwordHash").notNull(),
+  passwordSalt: text("passwordSalt").notNull(),
+  dateAdded: integer("dateAdded").notNull(),
+  dateLastUpdate: integer("dateLastUpdate"),
+  points: integer("points"),
+  roles: text("roles"),
+});
+
+export const personalInfo = sqliteTable("personalInfo", {
+  userID: text("userID")
+    .primaryKey()
+    .references(() => users.userID),
+  firstName: text("firstName").notNull(),
+  lastName: text("lastName").notNull(),
+  emails: text("emails").notNull(),
+  phoneNumber: integer("phoneNumber"),
+  phoneCountryCode: integer("phoneCountryCode"),
+});
+
+export const professionalInfo = sqliteTable("professionalInfo", {
+  userID: text("userID")
+    .primaryKey()
+    .references(() => users.userID),
+  resumePath: text("resumePath"),
+  linkedin: text("linkedin"),
+  portfolio: text("portfolio"),
+  majors: text("majors"),
+  minors: text("minors"),
+  graduationSemester: text("graduationSemester"),
+});
+
+export const saseInfo = sqliteTable("saseInfo", {
+  userID: text("userID")
+    .primaryKey()
+    .references(() => users.userID),
+  eventsAttended: text("eventsAttended"),
+  mentors: text("mentors"),
+  mentees: text("mentees"),
+  groups: text("groups"),
+});
