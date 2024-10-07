f73b388562238cc89f77d2fbe4d59d2cf034895f
Taebok Joseph Kim <josephkim7104@gmail.com>
2024-10-07 16:58:54 -0400

    v2

--- client/components/ui/navbar.tsx
+++ client/components/ui/navbar.tsx
@@ -1,18 +1,22 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link } from "@tanstack/react-router";
-import { IoMdMenu, IoMdClose } from "react-icons/io";
+import { FiUser } from "react-icons/fi";
+import { Squash as Hamburger } from 'hamburger-react';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
+  const hamburgerRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     const handleOutsideClick = (event: MouseEvent) => {
-      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target as Node) &&
+        hamburgerRef.current &&
+        !hamburgerRef.current.contains(event.target as Node)
+      ) {
         setMenuOpen(false);
       }
     };
@@ -22,65 +26,182 @@ const Navbar = () => {
 
   return (
     <header className="bg-white font-[Poppins] font-medium">
-      <nav className="flex justify-start items-center w-[95%] mx-8 my-3">
+      <nav className="flex justify-between items-center w-full px-4 md:px-8 py-3 h-16">
         <div>
           <Link to="/">
             <img
-              className="w-80 cursor-pointer transform hover:scale-105 inline-block duration-300"
+              className="w-40 md:w-44 cursor-pointer transform hover:scale-105 transition duration-300"
               src="./client/assets/images/SASELogo.png"
               alt="Logo"
             />
           </Link>
         </div>
 
-        <div
-          className={`nav-links md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 ${
-            menuOpen ? "w-full top-[8%] justify-end px-7 py-3" : "w-full top-[-100%] justify-end"
-          } flex px-4 duration-300`}>
-          <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+        {/* Desktop Navigation Buttons! */}
+        <div className="hidden md:flex items-center w-full">
+          <ul className="flex md:flex-row items-center gap-[4vw] mr-auto px-10 pt-1">
             <li>
-              <Link to="/" className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block" onClick={() => setMenuOpen(false)}>
+              <Link
+                to="/"
+                className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block"
+              >
                 Home
               </Link>
             </li>
             <li>
-              <Link to="/" className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block" onClick={() => setMenuOpen(false)}>
+              <Link
+                to="/"
+                className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block"
+              >
                 About
               </Link>
             </li>
             <li>
-              <Link to="/" className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block" onClick={() => setMenuOpen(false)}>
+              <Link
+                to="/"
+                className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block"
+              >
                 Board
               </Link>
             </li>
             <li>
-              <Link to="/" className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block" onClick={() => setMenuOpen(false)}>
+              <Link
+                to="/"
+                className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block"
+              >
                 Gallery
               </Link>
             </li>
             <li>
-              <Link to="/" className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block" onClick={() => setMenuOpen(false)}>
+              <Link
+                to="/"
+                className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block"
+              >
                 Events
               </Link>
             </li>
             <li>
-              <Link to="/" className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block" onClick={() => setMenuOpen(false)}>
+              <Link
+                to="/"
+                className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block"
+              >
+                Blogs
+              </Link>
+            </li>
+          </ul>
+          <div className="flex items-center gap-4">
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Search"
+                className="border border-gray-600 pl-10 pr-3 py-2 rounded-full focus:outline-none transition-all duration-300 ease-in-out w-32 focus:w-64 placeholder-gray-600"
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute left-3 top-3 h-5 w-5 text-gray-600 "
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm6-4l4 4"
+                />
+              </svg>
+            </div>
+            {isLoggedIn ? (
+              <Link to="/">
+                <button className="text-gray-600 px-1 py-2 rounded-full duration-300 hover:text-[#000000] transform hover:scale-105">
+                  <FiUser className="text-2xl" />
+                </button>
+              </Link>
+            ) : (
+              <Link to="/">
+                <button className="bg-[#0f6cb6] text-white px-5 py-2 rounded-full hover:bg-[#8dc63f] hover:text-[#000000] duration-300 transform hover:scale-105 inline-block">
+                  Log In
+                </button>
+              </Link>
+            )}
+          </div>
+        </div>
+
+        {/* Hamburger Menu */}
+        <div
+          ref={menuRef}
+          className={`nav-links md:hidden absolute bg-white transition-all ease-in-out duration-500 w-full ${
+            menuOpen ? "top-[65px]" : "top-[-100%]"
+          }`}
+        >
+          <ul className="flex flex-col items-end gap-8 py-4 pr-12">
+            <li>
+              <Link
+                to="/"
+                className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block"
+                onClick={() => setMenuOpen(false)}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block"
+                onClick={() => setMenuOpen(false)}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block"
+                onClick={() => setMenuOpen(false)}
+              >
+                Board
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block"
+                onClick={() => setMenuOpen(false)}
+              >
+                Gallery
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block"
+                onClick={() => setMenuOpen(false)}
+              >
+                Events
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="hover:text-[#0f6cb6] duration-300 transform hover:scale-105 inline-block"
+                onClick={() => setMenuOpen(false)}
+              >
                 Blogs
               </Link>
             </li>
           </ul>
         </div>
 
-        <div className="w-full flex items-center justify-end gap-4 ml-auto pr-4">
+        <div className="flex items-center gap-4 md:hidden">
           <div className="relative">
             <input
               type="text"
               placeholder="Search"
-              className="border border-gray-300 pl-10 pr-3 py-2 rounded-full focus:outline-none transition-all duration-300 ease-in-out w-32 focus:w-64"
+              className="border border-gray-600 pl-10 pr-3 py-2 rounded-full focus:outline-none transition-all duration-300 ease-in-out w-32 focus:w-64 placeholder-gray-600"
             />
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="absolute left-3 top-3 h-5 w-5 text-gray-400"
+              className="absolute left-3 top-3 h-5 w-5 text-gray-600 "
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -94,18 +215,21 @@ const Navbar = () => {
             </svg>
           </div>
 
-          <button className="bg-[#0f6cb6] text-white px-5 py-2 rounded-full hover:bg-[#8dc63f] hover:text-[#000000] duration-300 transform hover:scale-105 inline-block">
-            Log In
-          </button>
-        </div>
-
-        <div className="md:hidden flex items-center">
-          <button onClick={toggleMenu} className="text-gray-700 menu-btn">
-            {menuOpen ? (
-              <IoMdClose className="text-3xl cursor-pointer" />
-            ) : (
-              <IoMdMenu className="text-3xl cursor-pointer" />
-            )}
+          {isLoggedIn ? (
+            <Link to="/">
+              <button className="text-gray-600 px-1 py-2 rounded-full duration-300 hover:text-[#000000] transform hover:scale-105">
+                <FiUser className="text-2xl" />
+              </button>
+            </Link>
+          ) : (
+            <Link to="/">
+              <button className="bg-[#0f6cb6] text-white px-5 py-2 rounded-full hover:bg-[#8dc63f] hover:text-[#000000] duration-300 transform hover:scale-105 inline-block">
+                Log In
+              </button>
+            </Link>
+          )}
+          <button ref={hamburgerRef} className="focus:outline-none">
+            <Hamburger toggled={menuOpen} toggle={setMenuOpen} color="#000" size={22} />
           </button>
         </div>
       </nav>
