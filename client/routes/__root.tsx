cf7183e2e7f9765d50d569ae115de03420c7d59e
justin <justinlin@ufl.edu>
2024-10-14 16:20:51 -0400

    Fix type and formatting issues

--- client/routes/__root.tsx
+++ client/routes/__root.tsx
@@ -1,6 +1,6 @@
+import Navbar from "@client/components/Navbar";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import React, { Suspense } from "react";
-import Navbar from "@client/components/Navbar";
 
 const TanStackRouterDevtools =
   process.env.NODE_ENV === "production"
@@ -8,19 +8,17 @@ const TanStackRouterDevtools =
     : React.lazy(() =>
         import("@tanstack/router-devtools").then((res) => ({
           default: res.TanStackRouterDevtools,
-        }))
+        })),
       );
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Navbar />
-      <hr className="p-2"/>
-      <div className="h-12"/> {/* spacer for navbar*/}
-
+      <hr className="p-2" />
+      <div className="h-12" /> {/* spacer for navbar*/}
       {/* Content Rendering */}
       <Outlet />
-
       <Suspense>
         <TanStackRouterDevtools />
       </Suspense>
