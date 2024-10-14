0c4c244c7f650c3b7b85ec14bf647dcd5e2d8491
ssihala <ssihala@tutanota.com>
2024-10-14 15:33:17 -0400

    Add querying info based on user id

--- server/routes/userInfo.ts
+++ server/routes/userInfo.ts
@@ -2,13 +2,14 @@ import {
   insertPersonalSchema,
   insertProfessionalSchema,
 } from "@/shared/infoSchema";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
-// import { eq } from "drizzle-orm";
 import { db } from "../db";
 import * as Schema from "../db/schema";
 
 const infoRoutes = new Hono();
 
+//Insert personal info
 infoRoutes.post("/users/personal", async (c) => {
   const personalInfoInsertion = insertPersonalSchema.parse(await c.req.json());
   const personal_info = await db
@@ -17,6 +18,17 @@ infoRoutes.post("/users/personal", async (c) => {
   return c.json({ personal_info });
 });
 
+//Query personal info by user_id
+infoRoutes.get("/users/personal/:id", async (c) => {
+  const user_id = c.req.param("id");
+  const personal_info = await db
+    .select()
+    .from(Schema.personalInfo)
+    .where(eq(Schema.personalInfo.user_id, user_id));
+  return c.json({ personal_info });
+});
+
+//Insert professional info
 infoRoutes.post("/users/professional", async (c) => {
   const professionalInfoInsertion = insertProfessionalSchema.parse(
     await c.req.json(),
@@ -24,6 +36,16 @@ infoRoutes.post("/users/professional", async (c) => {
   const professional_info = await db
     .insert(Schema.professionalInfo)
     .values(professionalInfoInsertion);
+  return c.json({ professional_info });
+});
+
+//Query professional info by user_id
+infoRoutes.get("/users/professional/:id", async (c) => {
+  const user_id = c.req.param("id");
+  const professional_info = await db
+    .select()
+    .from(Schema.professionalInfo)
+    .where(eq(Schema.professionalInfo.user_id, user_id));
   return c.json({ professional_info });
 });
 
