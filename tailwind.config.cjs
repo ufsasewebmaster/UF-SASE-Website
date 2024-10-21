1501a019bb9af2671b3d90ab0369d707be2f96c1
Taebok Joseph Kim <josephkim7104@gmail.com>
2024-10-21 19:02:40 -0400

    Some edits

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -56,7 +56,7 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["Poppins", ...fontFamily.sans],
+        // sans: ["var(--font-sans)", ...fontFamily.sans],
       },
       keyframes: {
         "accordion-down": {
