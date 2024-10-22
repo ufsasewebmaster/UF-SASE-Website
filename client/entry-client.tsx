d76f48a466f6eb76e4a1d7b9adba2e6a35e3afec
Ethan Niser <ethan@niser.com>
2024-10-21 23:55:39 -0700

    fix preset

--- client/entry-client.tsx
+++ client/entry-client.tsx
@@ -11,29 +11,3 @@ ReactDOM.hydrateRoot(
   // todo: why does this error?
   <StartClient router={router as any} />,
 );
-
-export default new Proxy(
-  {},
-  {
-    get(target, property, receiver) {
-      console.log(
-        `Getting property "${property}" with value: ${target[property]}`,
-      );
-    },
-
-    // Trap for setting property values
-    set(target, property, value, receiver) {
-      console.log(`Setting property "${property}" to value: ${value}`);
-    },
-
-    // Trap for checking if a property exists
-    has(target, property) {
-      console.log(`Checking if property "${property}" exists`);
-    },
-
-    // Trap for deleting properties
-    deleteProperty(target, property) {
-      console.log(`Deleting property "${property}"`);
-    },
-  },
-);
