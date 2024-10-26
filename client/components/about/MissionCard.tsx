969fdcf720c4c9086b96a006f209504a53e9de6d
Taebok Joseph Kim <josephkim7104@gmail.com>
2024-10-25 21:05:20 -0400

    fixed size mission cards

--- client/components/about/MissionCard.tsx
+++ client/components/about/MissionCard.tsx
@@ -11,12 +11,12 @@ const MissionCard = ({ logo, shadowColor, text }: CardProps) => {
       : "hover:shadow-[12px_12px_0px_#7DC242]";
 
   return (
-    <div className="w-48 flex-none">
+    <div className="flex-none">
       <div
-        className={`flex min-h-48 w-48 transform flex-col items-center rounded-2xl border-2 border-black bg-gray-100 p-4 transition duration-300 hover:scale-105 ${hoverShadowClass}`}
+        className={`flex h-56 w-56 transform flex-col items-center rounded-2xl border-2 border-black bg-gray-100 p-4 transition duration-300 hover:scale-105 ${hoverShadowClass}`}
       >
         <img src={logo} alt="Logo" className="mb-2 h-10 w-10 object-contain" />
-        <p className="overflow-auto text-center text-black">{text}</p>
+        <p className="overflow-hidden text-center text-lg text-black">{text}</p>
       </div>
     </div>
   );
