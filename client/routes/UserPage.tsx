96269679c62d3d08f44aace70957e3bad69330cb
Taebok Joseph Kim <josephkim7104@gmail.com>
2024-10-27 14:49:53 -0400

    route fixes

--- client/routes/UserPage.tsx
+++ client/routes/UserPage.tsx
@@ -1,16 +1,16 @@
 // src/routes/UsersPage.tsx
-import { UserForm } from '@client/components/UserForm'
-import { UserList } from '@client/components/UserList'
-import { createFileRoute } from '@tanstack/react-router'
+import { UserForm } from "@client/components/UserForm";
+import { UserList } from "@client/components/UserList";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/UserPage')({
+export const Route = createFileRoute("/userpage")({
   component: () => {
     return (
       <div>
-        <h1 className="text-2xl font-bold mb-4">User Management</h1>
+        <h1 className="mb-4 text-2xl font-bold">User Management</h1>
         <UserForm />
         <UserList />
       </div>
-    )
+    );
   },
-})
+});
