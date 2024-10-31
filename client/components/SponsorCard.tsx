e4b722289d763be7ac9bc73dadb7b2d8ba934eb2
Stephanie Fong <fong.s@ufl.edu>
2024-10-31 15:25:28 -0400

    Updated sponsors page and added fonts to tailwind config

--- client/components/SponsorCard.tsx
+++ client/components/SponsorCard.tsx
@@ -1,32 +1,41 @@
 import { cn } from "@/shared/utils";
 
-const SponsorCard = (props: any) => {
+const SponsorCard = ({
+  companyName,
+  image,
+  type,
+}: {
+  image: string;
+  companyName: string;
+  type: string;
+}) => {
   return (
-    <div
-      className={cn(
-        {
-          "bg-gradient from-saseBlue to-white": props.diamond == "Y",
-          "bg-white": props.diamond == "N",
-        },
-        `flex flex-col items-center rounded-lg border-4 border-black`,
-      )}
-    >
-      <img
-        src={props.image}
-        alt="Company Logo"
-        className="h-3/4 w-full pl-1 pr-1 pt-1"
-      />
+    <div className="flex h-full w-full flex-col">
       <p
         className={cn(
           {
-            "text-white": props.diamond == "Y",
-            "text-black": props.diamond == "N",
+            "text-saseBlue": type == "Diamond",
+            "text-saseGreen": type == "Gold",
           },
-          `h-1/4 w-full p-4 text-center text-2xl font-medium`,
+          `font-redhat pb-2 text-center text-4xl font-semibold`,
         )}
       >
-        {props.companyName}
+        {type}
       </p>
+      <div className="relative flex h-auto w-full flex-col items-center rounded-2xl border-4 border-black bg-white">
+        <img
+          src={image}
+          alt="Company Logo"
+          className="h-5/6 w-full rounded-2xl pl-1 pr-1 pt-1"
+        />
+        <div className="flex w-full items-center justify-center">
+          <p className="font-redhat p-4 text-center text-3xl font-semibold">
+            {companyName}
+          </p>
+        </div>
+
+        <div className="absolute left-2 top-2 z-0 h-full w-full rounded-2xl border-b-8 border-r-8 border-saseGreen"></div>
+      </div>
     </div>
   );
 };
