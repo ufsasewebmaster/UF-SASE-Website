273f653f232bece7fdfe2f2905b376f186d56ad9
Ricky Zhang <rickyzhang196@outlook.com>
2024-11-01 17:24:06 -0400

    added auth context

--- client/entry-client.tsx
+++ client/entry-client.tsx
@@ -1,14 +1,42 @@
 /// <reference types="vinxi/types/client" />
-import { StartClient } from "@tanstack/start";
-import ReactDOM from "react-dom/client";
-import { createRouter } from "./router";
-import "./index.css";
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+import { createRouter } from './router'
+import './index.css'
+import { AuthProvider, useAuth } from './AuthContext'
+import { RouterProvider } from '@tanstack/react-router'
 
-const router = createRouter();
-const rootElement = document.getElementById("root");
+const router: ReturnType<typeof createRouter> = createRouter()
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+function InnerApp() {
+  const auth = useAuth()
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return <RouterProvider router={router as any} context={{ auth }} />
+}
+
+function App() {
+  return (
+    <AuthProvider>
+      <InnerApp />
+    </AuthProvider>
+  )
+}
+
+const rootElement = document.getElementById('root')
 
 if (rootElement) {
-  ReactDOM.hydrateRoot(rootElement, <StartClient router={router} />);
+  ReactDOM.hydrateRoot(
+    rootElement,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
 } else {
-  console.error("Root element not found");
+  console.error('Root element not found')
 }
---
+++ client/routerContext.tsx
@@ -0,0 +1,6 @@
+// src/routerContext.ts
+import type { AuthContextType } from "./AuthContext";
+
+export interface MyRouterContext {
+  auth: AuthContextType;
+}
