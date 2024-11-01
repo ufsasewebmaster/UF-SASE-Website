f5a51a9702bb90f87c4b198acad595695c9c6a21
eric wang <erichwang2005@gmail.com>
2024-11-01 19:50:57 -0400

    fixing errors with content searching

--- shared/blogContentSchema.ts
+++ shared/blogContentSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const contentSearchSchema = z.object({
-    content:z.string().min(1),
+  content: z.string().min(1),
 });
 
 export type searchBlogContent = z.infer<typeof contentSearchSchema>;
