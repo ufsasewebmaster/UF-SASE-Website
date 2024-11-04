7dc69ecedce3bb56b9145da2712ab2e200689544
Ricky Zhang <rickyzhang196@outlook.com>
2024-11-04 17:51:05 -0500

    added login

--- client/routes/gallery.tsx
+++ client/routes/gallery.tsx
@@ -1,9 +1,50 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/gallery")({
-  component: () => (
-    <div>
-        <div>Hello Gallery</div>
-    </div>
-  ),
+  component: () => {
+    return (
+      <div>
+        <div className="py-5"></div> {/* spacer element */}
+        <div className="flex justify-center">
+          <div className="flex-5 pb-5 text-center font-mono text-7xl">
+            GALLERY
+          </div>
+        </div>
+        <div className="flex w-full justify-center">
+          <hr className="h-5 w-10/12 border-t-4 border-saseBlue" />
+        </div>
+        <div className="flex items-center justify-center pb-8 pt-3">
+          <div className="w-1/12"></div> {/* spacer element */}
+          <div className="flex-1 pr-10 text-center">
+            <select
+              name="photo_drives"
+              id="photo_drives"
+              className="w-full rounded-lg border-2 border-black pb-1 pl-1 pt-1 font-mono text-lg font-semibold hover:cursor-pointer"
+            >
+              <option value="default" selected>
+                Photo Google Drives
+              </option>
+              <option value="Fall 2024">Fall 2024</option>
+              <option value="Spring 2024">Spring 2024</option>
+              <option value="Fall 2023">Fall 2023</option>
+              <option value="Spring 2023">Spring 2023</option>
+              <option value="Fall 2022">Fall 2022</option>
+            </select>
+          </div>
+          <div className="flex-1">{/* add image eventually */}</div>
+        </div>
+        <div className="flex w-full justify-center">
+          <hr className="h-5 w-10/12 border-t-4 border-saseGreen" />
+        </div>
+        <div className="justify-center">
+          <div className="flex-5 pb-5 pt-5 text-center font-mono text-3xl">
+            Spring 2024
+          </div>
+        </div>
+        <div id="slideshow" className="border-black">
+          {/* slideshow component goes in this div*/}
+        </div>
+      </div>
+    );
+  },
 });
--- client/routes/login.tsx
+++ client/routes/login.tsx
@@ -1,5 +1,123 @@
+import { useMutation } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { useForm } from "react-hook-form";
+import { Logo } from "@navigation/Logo";
+import { Page } from "@components/Page";
+import { Button } from "@ui/button";
+import { Input } from "@ui/input";
+
+interface FormData {
+  username: string;
+  password: string;
+}
 
 export const Route = createFileRoute("/login")({
-  component: () => <div>Hello /Login!</div>,
+  component: () => {
+    const {
+      formState: { errors },
+      handleSubmit,
+      register,
+    } = useForm<FormData>();
+
+    const mutation = useMutation({
+      mutationFn: async (formData: FormData) => {
+        const response = await fetch(
+          "https://sheetdb.io/api/v1/79fo2g87zoqgy",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+          },
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        return response.json();
+      },
+      onSuccess: () => {
+        console.log("Data submitted successfully!");
+      },
+      onError: (error) => {
+        console.error("Error submitting data:", error);
+      },
+    });
+
+    const onSubmit = (data: FormData) => {
+      mutation.mutate(data);
+    };
+
+    return (
+      <Page>
+        <div className="relative flex min-h-screen items-center justify-center">
+          {/* Shadow Card 1 */}
+          <div className="absolute left-1/2 z-0 -ml-4 mt-6 h-[32rem] w-full max-w-md -translate-x-1/2 -translate-y-6 transform rounded-lg bg-saseGreen opacity-100 shadow-lg"></div>
+
+          {/* Shadow Card 2 (Right) */}
+          <div className="absolute right-1/2 z-0 -mr-4 mt-20 h-[31.5rem] w-full max-w-md -translate-y-6 translate-x-1/2 transform rounded-lg bg-saseBlue opacity-100 shadow-lg"></div>
+
+          {/* Login Form Container */}
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="border-gray relative z-10 flex h-[32rem] w-full max-w-md flex-col items-center justify-center rounded-lg border bg-gray-100 p-6 shadow-xl"
+          >
+            <div className="mb-6 p-2">
+              <Logo />
+            </div>
+            <h3 className="heading mb-3 pb-2 text-center font-redhat text-4xl font-semibold">
+              Login
+            </h3>
+            <p className="mb-4 text-center text-lg"> Sign in to your account</p>
+
+            <Input
+              type="text"
+              {...register("username", {
+                required: "Username is required",
+                maxLength: {
+                  value: 12,
+                  message: "Username must be 12 characters or fewer!",
+                },
+              })}
+              placeholder="Username"
+              className={`mb-4 rounded-lg border border-gray-300 bg-saseGreen p-5 placeholder-black opacity-90 ${errors.username ? "border-red-600" : ""}`}
+            />
+            {errors.username && (
+              <span className="errorMessage mb-4 text-red-600">
+                {errors.username.message}
+              </span>
+            )}
+
+            <Input
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+                pattern: {
+                  value: /^(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/,
+                  message:
+                    "Password must be longer than 8 characters, contain a number and a special character!",
+                },
+              })}
+              placeholder="Password"
+              className={`mb-4 rounded-lg border border-gray-300 bg-saseGreen p-5 placeholder-black opacity-90 ${errors.password ? "border-red-600" : ""}`}
+            />
+            {errors.password && (
+              <span className="errorMessage mb-4 text-red-600">
+                {errors.password.message}
+              </span>
+            )}
+
+            <Button
+              type="submit"
+              className="w-full rounded-lg border border-gray-300 bg-saseBlue p-5 text-white"
+            >
+              Login
+            </Button>
+          </form>
+        </div>
+      </Page>
+    );
+  },
 });
