c40b47d92ae3821937ebc351d0085179b8ec0e35
ananya0817 <ananya.mundrathi@gmail.com>
2024-11-08 23:13:58 -0500

    board page
    added board images. fixed some functionality. made board member cards larger. waiting for description.

--- client/routes/board.tsx
+++ client/routes/board.tsx
@@ -1,4 +1,4 @@
-import boardGroup from "@assets/board/placeholder.png";
+import boardGroup from "@assets/board/boardpic.jpeg";
 import boardInfo from "@components/BoardInfo";
 import BoardMemberCard from "@components/BoardMemberCard";
 import { createFileRoute } from "@tanstack/react-router";
@@ -6,15 +6,15 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/board")({
   component: () => {
     return (
-      <div className="min-h-screen bg-gray-50 px-4 py-8 font-[Poppins] md:px-16">
+      <div className="min-h-screen px-4 py-8 font-[Poppins] md:px-16">
         {/* title */}
         <div className="text-center">
-          <h1 className="text-4xl font-bold text-gray-800">Board</h1>
-          <p className="mt-4 text-xl font-bold text-gray-600">
+          <h1 className="font-oswald text-7xl font-medium">BOARD</h1>
+          <p className="mt-8 mb-8 text-2xl text-black">
             Meet our 2023-2024 SASE Board Members!
           </p>
           <hr className="w-7/8 mx-auto my-4 border-t-2 border-green-500" />
-        </div>
+          </div>
 
         {/* group picture */}
         <div className="mb-8 mt-10 flex justify-center font-[Poppins]">
@@ -22,11 +22,14 @@ export const Route = createFileRoute("/board")({
             <img src={boardGroup} className="w-full" />
           </div>
         </div>
-        <hr className="w-7/8 mx-auto my-2 mt-10 border-t-2 border-blue-500" />
+        <hr className="w-7/8 mb-10 mt-16 border-t-2 border-blue-500" />
 
         {boardInfo.map((section, idx) => (
           <div key={idx} className="mb-12">
-            <h2 className="mb-6 text-center text-3xl font-semibold">
+            {section.section === "Chair Board" && (
+              <hr className="w-7/8 my-10 border-t-2 border-green-500" />
+            )}
+            <h2 className="mb-6 font-oswald text-center text-5xl ">
               {section.section}
             </h2>
             <div className="flex flex-wrap justify-center gap-10">
