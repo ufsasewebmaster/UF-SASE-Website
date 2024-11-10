1d22da467cc8b733a98ed39caf0f16f94779d19c
zouhelen <helenzou05@gmail.com>
2024-11-10 15:58:24 -0500

    fix positioning of calendar, drop shadow color, and add page title

--- client/routes/events.tsx
+++ client/routes/events.tsx
@@ -4,9 +4,20 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/events")({
   component: () => {
     return (
-      <>
-        <EventsCalendar />
-      </>
+      <div>
+        <div className="py-5"></div> {/* spacer element */}
+        <div className="flex justify-center">
+          <div className="flex-5 pb-5 text-center font-mono text-7xl">
+            EVENTS & SLIDES
+          </div>
+        </div>
+        <div className="flex w-full justify-center">
+          <hr className="h-5 w-10/12 border-t-4 border-saseBlue" />
+        </div>
+        <div className="flex w-full justify-center">
+          <EventsCalendar />
+        </div>
+      </div>
     );
   },
 });
