86989a7d1ec2a420fd97ebb284b5cda0490ccdfa
Ricky Zhang <rickyzhang196@outlook.com>
2024-11-13 22:49:24 -0500

    Image button

--- src/client/components/EventsCalendar.tsx
+++ src/client/components/EventsCalendar.tsx
@@ -2,20 +2,12 @@ const EventsCalendar = () => (
   <div className="relative w-4/5">
     <div
       className="absolute left-3 top-3 h-full w-full rounded-lg bg-blueShadow"
-      style={{ width: "60%", height: "550px", minWidth: "550px", zIndex: 1 }}
+      style={{ width: "60%", height: "550px", minWidth: "550px" }} // Removed zIndex
     ></div>
 
     <iframe
       src="https://calendar.google.com/calendar/embed?src=47b916a8ac315c88c4dc86bb240cbd1bf1bfdae1800445aa534d7fd1818badcf%40group.calendar.google.com&ctz=America%2FNew_York"
-      style={{
-        border: "2px solid black",
-        borderRadius: "15px",
-        zIndex: 2,
-        position: "relative",
-        width: "60%",
-        height: "550px",
-        minWidth: "550px",
-      }}
+      className="relative w-[60%] h-[550px] min-w-[550px] border-2 border-black rounded-lg z-10" // Adjusted zIndex if needed
       frameBorder="0"
       scrolling="no"
     ></iframe>
--- src/client/routes/events.tsx
+++ src/client/routes/events.tsx
@@ -1,7 +1,11 @@
-import saseLogo from "@/client/assets/SASELogo.png";
+// client/routes/events.tsx
+import React from "react";
 import EventsCalendar from "@components/EventsCalendar";
 import { createFileRoute } from "@tanstack/react-router";
+import ImageButton from "../components/events/ImageButton";
+import peopleImage from "@assets/about/people.png"; // Adjust the path as needed
 import { seo } from "../utils/seo";
+import saseLogo from "@assets/SASELogo.png";
 
 export const Route = createFileRoute("/events")({
   meta: () => [
@@ -15,16 +19,15 @@ export const Route = createFileRoute("/events")({
     return (
       <div>
         <div className="py-5"></div> {/* spacer element */}
-        <div className="flex justify-center">
-          <div className="flex-5 pb-5 text-center font-mono text-7xl">
-            EVENTS & SLIDES
-          </div>
+        <div className="flex-5 pb-5 justify-center text-center font-mono text-7xl">
+          EVENTS & SLIDES
         </div>
         <div className="flex w-full justify-center">
           <hr className="h-5 w-10/12 border-t-4 border-saseBlue" />
         </div>
         <div className="flex w-full justify-center">
           <EventsCalendar />
+          <ImageButton imageUrl={peopleImage} />
         </div>
       </div>
     );
