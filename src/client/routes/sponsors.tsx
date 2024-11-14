dbf68eac4c4fe14833c77cb5ca67bb0f37a11850
Stephanie Fong <fong.s@ufl.edu>
2024-11-14 16:13:36 -0500

    i lied, going to continue home page draft on this pr

--- src/client/routes/sponsors.tsx
+++ src/client/routes/sponsors.tsx
@@ -56,8 +56,13 @@ export const Route = createFileRoute("/sponsors")({
             <p className="absolute bottom-[10%] pl-4 pr-4 text-center font-redhat text-base font-medium italic text-white md:text-xl lg:text-2xl xl:text-3xl">
               To view our sponsorship packet, or for any related questions,
               please contact our External Vice President, Kayleen Diaz, at{" "}
-              <span className="text-saseGreen underline">
-                ufsase.evp@gmail.com
+              <span>
+                <a
+                  href={`mailto:ufsase.evp@gmail.com`}
+                  className="text-saseGreen underline"
+                >
+                  ufsase.evp@gmail.com
+                </a>
               </span>
               .
             </p>
