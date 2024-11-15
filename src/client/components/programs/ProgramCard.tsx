06d221b9de1a5c6c2f8d62749a617ba3f5b68e79
Taebok Joseph Kim <josephkim7104@gmail.com>
2024-11-15 16:48:05 -0500

    shadow color change

--- src/client/components/programs/ProgramCard.tsx
+++ src/client/components/programs/ProgramCard.tsx
@@ -10,7 +10,7 @@ const ProgramCard: React.FC<ProgramCardProps> = ({ image, link, text }) => {
   return (
     <div className="relative mb-24 w-full max-w-6xl p-6">
       {/* Main Card with Black Border and Green Shadow */}
-      <div className="relative flex transform flex-col items-center rounded-3xl border-2 border-black bg-gray-100 p-12 shadow-[16px_16px_0px_#7DC242] transition duration-300 hover:shadow-[24px_24px_0px_#7DC242] sm:flex-row">
+      <div className="relative flex transform flex-col items-center rounded-3xl border-2 border-black bg-gray-100 p-12 shadow-[16px_16px_0px_#7DC242] transition duration-300 hover:shadow-[24px_24px_0px_#0668B3] sm:flex-row">
         {/* Image */}
         <div className="mb-4 flex-shrink-0 sm:mb-0 sm:mr-8">
           <img
