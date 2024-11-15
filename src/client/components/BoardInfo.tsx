03a74aa365987da82736819ec5d1f4a7506942c9
ananya0817 <ananya.mundrathi@gmail.com>
2024-11-14 22:52:26 -0500

    board page
    added descriptions & fixed it so you can close out of the member card anywhere you click.

--- src/client/components/BoardInfo.tsx
+++ src/client/components/BoardInfo.tsx
@@ -31,7 +31,8 @@ const boardInfo = [
         image: v_l,
         major: "3rd Year Computer Science",
         contact: "ufsase.president@gmail.com",
-        description: "description",
+        description:
+          "I lead a team of 21 officers to plan and execute 70+ annual events for 600+ members. To ensure quality events, I help oversee an annual budget of $20,000+ to fund resources that foster leadership and growth. I coordinated a trip of 80+ members to SASE National Convention and managed $30,000 to provide students with valuable professional networking opportunities.",
       },
       {
         name: "Bryan Park",
@@ -39,7 +40,8 @@ const boardInfo = [
         image: b_p,
         major: "3rd Year Mechanical Engineering",
         contact: "ufsase.vp@gmail.com",
-        description: "description",
+        description:
+          "I've really enjoyed my time in SASE and I wanted to continue giving back to our amazing SASE community. I love my mentor-mentee group (shoutout to the Park-Rangers) and I wanted to help others make meaningful connections through SASE. ",
       },
       {
         name: "Kayleen Diaz",
@@ -47,7 +49,8 @@ const boardInfo = [
         image: k_d,
         major: "3rd Year Mechanical Engineering",
         contact: "ufsase.evp@gmail.com",
-        description: "description",
+        description:
+          "I have the wonderful role of leading SASE's professional development! Whether it be partnering with companies for workshops or creating meaningful external relationships, I hope to provide SASE members with opportunities to further their careers. As EVP, I hope to give back to the SASE community that has helped me grow so much! ",
       },
       {
         name: "Gayatri Baskaran",
@@ -55,7 +58,8 @@ const boardInfo = [
         image: g_b,
         major: "3rd Year Computer Engineering",
         contact: "ufsase.treasurer@gmail.com",
-        description: "description",
+        description:
+          "By taking care of SASE's finances, I want to make sure our board members and committees can focus on making our events the best that they can be. SASE is special to me because the positivity and inclusivity of our community is unmatched. There's always someone to yap with!",
       },
       {
         name: "Rachel Young",
@@ -63,7 +67,8 @@ const boardInfo = [
         image: r_y,
         major: "3nd Year Computer Science",
         contact: "ufsase.secretary@gmail.com",
-        description: "description",
+        description:
+          "I handle space reservations for all SASE events, communicating with UF’s registrar to secure suitable rooms. I also moderate the UF SASE Discord server, including roles, channels, and resources. I update our Google Calendar and Discord Events tab with upcoming events and deadlines, and I oversee our weekly board meetings.",
       },
       {
         name: "Kimmy Chu",
@@ -71,7 +76,8 @@ const boardInfo = [
         image: k_c,
         major: "2nd Year Environmental Engineering",
         contact: "ufsase.pr@gmail.com",
-        description: "description",
+        description:
+          "SASE has been a welcoming community that supported my professional development, so I chose to give back by becoming a PR Officer. In this role, I manage the official UF SASE Instagram, Facebook, and Linktree accounts, serve as the main contact for external marketing, and oversee all marketing logistics.",
       },
     ],
   },
@@ -79,20 +85,13 @@ const boardInfo = [
     section: "Chair Board",
     members: [
       {
-        name: "Ricky Zhang",
-        role: "Webmaster",
-        image: r_z,
-        major: "2nd Year Computer Science",
-        contact: "ufsase.webmaster@gmail.com",
-        description: "description",
-      },
-      {
         name: "Amanda Jiang",
         role: "Advancement",
         image: a_j,
         major: "2nd Year Computer Science",
         contact: "ufsase.academic@gmail.com",
-        description: "description",
+        description:
+          "Hi! My name is Yangying (Amanda), and I'm SASE's Advancement Chair for the 2024-2025 school year. I’m in charge of the organization’s academic and professional development, so I help host events like resume workshops, resume hours, and board office hours. Want to learn more? Come to one of our GBMs!",
       },
       {
         name: "Chloe Bai",
@@ -100,7 +99,8 @@ const boardInfo = [
         image: c_b,
         major: "2nd Year Computer Science",
         contact: "ufsase.fundraising@gmail.com",
-        description: "description",
+        description:
+          "I joined SASE because of the amazing friends and community I found here. I wanted to give back to SASE and help make it the same kind of place for others. As Fundraising Chair, I organize a variety of fundraisers from food to headshots. I am also responsible for coordinating concessions in collaboration with HEAL.",
       },
       {
         name: "Anna Lim",
@@ -108,7 +108,8 @@ const boardInfo = [
         image: an_l,
         major: "2nd Year Biochemistry & Music Performance",
         contact: "ufsase.moc@gmail.com",
-        description: "description",
+        description:
+          "In my role as Co-Member Involvement Chair, I work with my co-chair, Justin, to organize SASE’s leadership and professional development program, SASE Interns. It’s incredibly rewarding to support our 90+ Interns as they gain opportunities to plan their own events, shadow board members to learn about positional responsibilities, engage with peers, and grow as professionals and leaders.",
       },
       {
         name: "Justin Doan",
@@ -116,7 +117,8 @@ const boardInfo = [
         image: j_d,
         major: "2nd Year Environmental Engineering",
         contact: "ufsase.moc@gmail.com",
-        description: "description",
+        description:
+          "As Co-Member-Involvement Chair, I work with my co-chair, Anna, to lead and direct SASE's leadership and professional development program, SASE Interns. With 90+ interns, we are in charge of creating the programs structure and events, including weekly meetings, board presentations/shadowing, Board x Interns event, interns-only socials, and ensuring overall success of the program.",
       },
       {
         name: "Helen Zou",
@@ -124,7 +126,8 @@ const boardInfo = [
         image: h_z,
         major: "2nd Year Computer Engineering",
         contact: "ufsase.historian@gmail.com",
-        description: "description",
+        description:
+          "As historian, my goal is to capture all the fun times of SASE so that members can look back on their experiences fondly and share them. My responsibilities include documenting all SASE events through photos/videos and editing videos such as recaps.",
       },
       {
         name: "Adriel Poon",
@@ -132,7 +135,8 @@ const boardInfo = [
         image: a_p,
         major: "2nd Year Computer Engineering",
         contact: "ufsase.multimedia@gmail.com",
-        description: "description",
+        description:
+          "I wanted to be a part of SASE because of its welcoming community. I applied for Multimedia chair because I enjoyed creating merch. I’m mainly inspired by streamers who occasionally have merch drops of their brand and hoped to implement a smaller scale version into each semester! I also love making graphics and scrolling through Pinterest and Instagram for references!",
       },
       {
         name: "Reyna Simpson",
@@ -140,7 +144,8 @@ const boardInfo = [
         image: r_s,
         major: "2nd Year Material Science Engineering",
         contact: "ufsase.multimedia@gmail.com",
-        description: "description",
+        description:
+          "SASE has such a warm and welcoming community that I wanted to be a part of it. Therefore, I wanted to be multimedia chair because I wanted to be more involved while doing something I enjoy. Through this position, I have fun creating graphics and merchandise for my SASE chapter.",
       },
       {
         name: "Tabitha Gottipati",
@@ -148,7 +153,8 @@ const boardInfo = [
         image: t_g,
         major: "2nd Year Mechanical Engineering",
         contact: "ufsase.alumni@gmail.com",
-        description: "description",
+        description:
+          "I joined SASE because of how easy it was to make friends! In addition, the quality of SASE events and the effort of the officers to be inclusive stuck out to me. As networking chair my main responsibility is getting an alumni spotlight for each GBM so our wonderful members can learn from their experiences at UF.",
       },
       {
         name: "Aahan Dwivedi",
@@ -156,7 +162,8 @@ const boardInfo = [
         image: a_d,
         major: "3rd Year Physics & Math",
         contact: "ufsase.service@gmail.com",
-        description: "description",
+        description:
+          "I create resources, opportunities, and workshops focused on research, graduate school and outreach to ensure that SASE is better meeting science members' needs. I joined SASE because of its community and hope that it has the tools to enable members to succeed in all career paths.",
       },
       {
         name: "Leann Tang",
@@ -164,7 +171,8 @@ const boardInfo = [
         image: l_t,
         major: "2nd Year Mechanical Engineering",
         contact: "ufsase.service@gmail.com",
-        description: "description",
+        description:
+          "As Service Chair, I am responsible for planning service events and initiatives that benefit the local Gainesville community. This means reaching out to local volunteer organizations to plan fun and engaging service events for SASE members. In addition, I collaborate with HEAL for our semesterly Adopt-a-Street clean ups.",
       },
       {
         name: "Sophia Dong",
@@ -172,7 +180,8 @@ const boardInfo = [
         image: s_d,
         major: "3rd Year Industrial and Systems Engineering",
         contact: "ufsase.social@gmail.com",
-        description: "description",
+        description:
+          "As the Social Chair for SASE, I’ve been drawn to the strong sense of community it fosters through sports, socials, and informational events. My role allows me to create engaging events for our general body, honing skills in event programming and building connections. I’m grateful for the mentorship and resources along the way!",
       },
       {
         name: "Jessica Lu",
@@ -180,7 +189,8 @@ const boardInfo = [
         image: j_l,
         major: "2nd Year Chemical Engineering",
         contact: "ufsase.sports@gmail.com",
-        description: "description",
+        description:
+          "Managing the SASE Sports Program, which includes overseeing intramurals, casual sports, and other sports events.",
       },
       {
         name: "Alexander Lou",
@@ -188,7 +198,8 @@ const boardInfo = [
         image: al_l,
         major: "2nd Year Computer Science",
         contact: "ufsase.sports@gmail.com",
-        description: "description",
+        description:
+          "Managing the SASE Sports Program, which includes overseeing intramurals, casual sports, and other sports events.",
       },
       {
         name: "Manav Sanghvi",
@@ -196,7 +207,8 @@ const boardInfo = [
         image: m_s,
         major: "2nd Year Computer Science",
         contact: "ufsase.tech@gmail.com",
-        description: "description",
+        description:
+          "I'm involved in SASE because of the incredible community and professional development it's given me. As Technical Chair, I contribute directly to the technical and professional development of 30+ members in the SASE Engineering Team (SET). This year, we are designing and developing a search-and-rescue robotic dog!",
       },
       {
         name: "Kevin Tang",
@@ -204,7 +216,17 @@ const boardInfo = [
         image: k_t,
         major: "2nd Year Mechanical Engineering",
         contact: "ufsase.tech@gmail.com",
-        description: "description",
+        description:
+          "As one of the technical chairs, I am leading the SASE Engineering Team (SET), a group of 33 talented members, in the development of a custom built robotic dog. I also hosted the Fall 2024 Technical Workshop, where SASE members learned how to use SolidWorks and 3D printing.",
+      },
+      {
+        name: "Ricky Zhang",
+        role: "Webmaster",
+        image: r_z,
+        major: "2nd Year Computer Science",
+        contact: "ufsase.webmaster@gmail.com",
+        description:
+          "As webmaster, my primary responsibilities are leading the Web Development Team and hosting computer-science related workshops such as practical programming. I really wanted to improve our slow, outdated website to reflect all the amazing things SASE has to offer!",
       },
     ],
   },
