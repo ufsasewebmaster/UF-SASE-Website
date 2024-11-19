8fd8529ec86b01a30d384ed3d2bcb75dfb5b5937
UF SASE Webmaster <160655323+ufsasewebmaster@users.noreply.github.com>
2024-11-19 18:12:03 -0500

    Es2 (#120)
    * fix image htings
    
    * add react icons
    
    * add react icons
    
    * finally added missing package...
    
    ---------
    
    Co-authored-by: khushichitalia <kjchitalia@gmail.com>
    Co-authored-by: Ricky Zhang <rickyzhang196@outlook.com>

---
+++ src/client/components/events/ImageButton.tsx
@@ -0,0 +1,37 @@
+import Modal from "@/client/components/Modal";
+import React, { useState } from "react";
+
+interface ImageButtonProps {
+  imageUrl: string;
+  buttonLabel?: string;
+  slideUrl: string;
+}
+
+const ImageButton: React.FC<ImageButtonProps> = ({ imageUrl, slideUrl }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <div>
+      <img
+        src={imageUrl}
+        onClick={openModal}
+        className="max-w-sm cursor-pointer px-40 py-10 hover:opacity-90 focus:outline-none"
+      />
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          onClose={closeModal}
+          title="Image Preview"
+          description="This is a preview of the selected image."
+        >
+          <iframe src={slideUrl} width="960" height="569"></iframe>
+        </Modal>
+      )}
+    </div>
+  );
+};
+
+export default ImageButton;
--- src/client/routes/events.tsx
+++ src/client/routes/events.tsx
@@ -1,6 +1,8 @@
-import saseLogo from "@/client/assets/SASELogo.png";
+import ExampleSlideShow from "@assets/events/ExampleSlideShow.png";
+import saseLogo from "@assets/SASELogo.png";
 import EventsCalendar from "@components/EventsCalendar";
 import { createFileRoute } from "@tanstack/react-router";
+import ImageButton from "../components/events/ImageButton";
 import { seo } from "../utils/seo";
 
 export const Route = createFileRoute("/events")({
@@ -23,8 +25,34 @@ export const Route = createFileRoute("/events")({
         <div className="flex w-full justify-center">
           <hr className="h-5 w-10/12 border-t-4 border-saseBlue" />
         </div>
-        <div className="flex w-full justify-center">
+        <div className="flex items-center justify-center">
           <EventsCalendar />
+        </div>
+        <div className="grid grid-cols-3 justify-center gap-4">
+          <ImageButton
+            slideUrl="https://docs.google.com/presentation/d/e/2PACX-1vRZwx9OSENEgIJfkpJ4M_1ttXGPrBcP6UTxpgGGGi7J4CYEGaDZY2kKaUdJLRhwq7o9hSHf9vU9LX5s/embed?start=false&loop=false&delayms=3000"
+            imageUrl={ExampleSlideShow}
+          />
+          <ImageButton
+            slideUrl="https://docs.google.com/presentation/d/e/2PACX-1vRZwx9OSENEgIJfkpJ4M_1ttXGPrBcP6UTxpgGGGi7J4CYEGaDZY2kKaUdJLRhwq7o9hSHf9vU9LX5s/embed?start=false&loop=false&delayms=3000"
+            imageUrl={ExampleSlideShow}
+          />
+          <ImageButton
+            slideUrl="https://docs.google.com/presentation/d/e/2PACX-1vRZwx9OSENEgIJfkpJ4M_1ttXGPrBcP6UTxpgGGGi7J4CYEGaDZY2kKaUdJLRhwq7o9hSHf9vU9LX5s/embed?start=false&loop=false&delayms=3000"
+            imageUrl={ExampleSlideShow}
+          />
+          <ImageButton
+            slideUrl="https://docs.google.com/presentation/d/e/2PACX-1vRZwx9OSENEgIJfkpJ4M_1ttXGPrBcP6UTxpgGGGi7J4CYEGaDZY2kKaUdJLRhwq7o9hSHf9vU9LX5s/embed?start=false&loop=false&delayms=3000"
+            imageUrl={ExampleSlideShow}
+          />
+          <ImageButton
+            slideUrl="https://docs.google.com/presentation/d/e/2PACX-1vRZwx9OSENEgIJfkpJ4M_1ttXGPrBcP6UTxpgGGGi7J4CYEGaDZY2kKaUdJLRhwq7o9hSHf9vU9LX5s/embed?start=false&loop=false&delayms=3000"
+            imageUrl={ExampleSlideShow}
+          />
+          <ImageButton
+            slideUrl="https://docs.google.com/presentation/d/e/2PACX-1vRZwx9OSENEgIJfkpJ4M_1ttXGPrBcP6UTxpgGGGi7J4CYEGaDZY2kKaUdJLRhwq7o9hSHf9vU9LX5s/embed?start=false&loop=false&delayms=3000"
+            imageUrl={ExampleSlideShow}
+          />
         </div>
       </div>
     );
