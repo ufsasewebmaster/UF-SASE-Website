cf07ec0229a2c1446115b44a233ad19db81cd606
Ricky Zhang <rickyzhang196@outlook.com>
2024-11-19 17:30:02 -0500

    Omg I can't believe I rebased this successfully

--- src/client/components/GalleryDropdown.tsx
+++ src/client/components/GalleryDropdown.tsx
@@ -1,4 +1,5 @@
-import React, { SetStateAction, useState } from "react";
+import type { SetStateAction} from "react";
+import React, { useCallback, useState } from "react";
 import { CgPlayButtonR } from "react-icons/cg";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa6";
 import dropdownImage from "../assets/gallery/dropdown_image.webp";
@@ -8,12 +9,17 @@ interface DropdownProps {
   setSlideshow: React.Dispatch<SetStateAction<string>>;
 }
 
-const GalleryDropdown: React.FC<DropdownProps> = ({ setSlideshow }) => {
+const GalleryDropdown: React.FC<DropdownProps> = React.memo(({ setSlideshow }) => {
   const [isDropdownVisible, setIsDropdownVisible] = useState(true);
 
-  const toggleVisibility = () => {
+  // Memoized callback functions to avoid re-renders
+  const toggleVisibility = useCallback(() => {
     setIsDropdownVisible((prev) => !prev);
-  };
+  }, []);
+
+  const setFall2024 = useCallback(() => setSlideshow("Fall 2024"), [setSlideshow]);
+  const setSpring2024 = useCallback(() => setSlideshow("Spring 2024"), [setSlideshow]);
+  const setFall2023 = useCallback(() => setSlideshow("Fall 2023"), [setSlideshow]);
 
   return (
     <div className="items-top flex w-full justify-between pb-10">
@@ -24,69 +30,13 @@ const GalleryDropdown: React.FC<DropdownProps> = ({ setSlideshow }) => {
           onClick={toggleVisibility}
         >
           <div>Photo Google Drives</div>
-          <div className="pr-3">
-            {isDropdownVisible ? <FaChevronDown /> : <FaChevronUp />}
-          </div>
+          <div className="pr-3">{isDropdownVisible ? <FaChevronDown /> : <FaChevronUp />}</div>
         </div>
-        <div className="w-1/12 pt-5"></div> {/* spacer element */}
         {isDropdownVisible && (
-          <div
-            id="photo_drives"
-            className="w-full rounded-lg border-2 border-black pb-1 pl-2 pt-1 text-left font-redhat text-lg font-normal shadow-[5px_5px_0px_0px_rgb(125,194,66)]"
-          >
-            <div>
-              <div className="flex items-center py-1">
-                <p>Fall 2024</p>
-                <div className="p-1"></div> {/* spacer element */}
-                <div>
-                  <div
-                    className="flex cursor-pointer items-center justify-center rounded-full border-2 border-gray-700 p-1 hover:opacity-45"
-                    onClick={() => {
-                      setSlideshow("Fall 2024");
-                    }}
-                  >
-                    <div className="px-0.5">
-                      <CgPlayButtonR size={10} />{" "}
-                    </div>
-                    <p className="text-xs">Slideshow</p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex items-center py-1">
-                <p>Spring 2024</p>
-                <div className="p-1"></div> {/* spacer element */}
-                <div>
-                  <div
-                    className="flex cursor-pointer items-center justify-center rounded-full border-2 border-gray-700 p-1 hover:opacity-45"
-                    onClick={() => {
-                      setSlideshow("Spring 2024");
-                    }}
-                  >
-                    <div className="px-0.5">
-                      <CgPlayButtonR size={10} />{" "}
-                    </div>
-                    <p className="text-xs">Slideshow</p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex items-center py-1">
-                <p>Fall 2023</p>
-                <div className="p-1"></div> {/* spacer element */}
-                <div>
-                  <div
-                    className="flex cursor-pointer items-center justify-center rounded-full border-2 border-gray-700 p-1 hover:opacity-45"
-                    onClick={() => {
-                      setSlideshow("Fall 2023");
-                    }}
-                  >
-                    <div className="px-0.5">
-                      <CgPlayButtonR size={10} />{" "}
-                    </div>
-                    <p className="text-xs">Slideshow</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="w-full rounded-lg border-2 border-black pb-1 pl-2 pt-1 text-left font-redhat text-lg font-normal shadow-[5px_5px_0px_0px_rgb(125,194,66)]">
+            <DropdownItem label="Fall 2024" onClick={setFall2024} />
+            <DropdownItem label="Spring 2024" onClick={setSpring2024} />
+            <DropdownItem label="Fall 2023" onClick={setFall2023} />
           </div>
         )}
       </div>
@@ -94,11 +44,29 @@ const GalleryDropdown: React.FC<DropdownProps> = ({ setSlideshow }) => {
         <img
           src={dropdownImage}
           alt=""
+          loading="lazy"
           className="h-56 min-h-[150px] w-96 min-w-[300px] rounded-lg border-2 border-black object-cover"
         />
       </div>
     </div>
   );
-};
+});
+
+// Helper component for dropdown items
+const DropdownItem: React.FC<{ label: string; onClick: () => void }> = ({ label, onClick }) => (
+  <div className="flex items-center py-1">
+    <p>{label}</p>
+    <div className="p-1"></div>
+    <div
+      className="flex cursor-pointer items-center justify-center rounded-full border-2 border-gray-700 p-1 hover:opacity-45"
+      onClick={onClick}
+    >
+      <div className="px-0.5">
+        <CgPlayButtonR size={10} />
+      </div>
+      <p className="text-xs">Slideshow</p>
+    </div>
+  </div>
+);
 
 export default GalleryDropdown;
--- src/client/routes/gallery.tsx
+++ src/client/routes/gallery.tsx
@@ -1,10 +1,10 @@
 import saseLogo from "@/client/assets/SASELogo.png";
 import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoMdLink } from "react-icons/io";
 import GalleryDropdown from "../components/GalleryDropdown";
-import { seo } from "../utils/seo";
 import GalleryZipExtraction from "../components/GalleryZipExtraction";
+import { seo } from "../utils/seo";
 
 export const Route = createFileRoute("/gallery")({
   meta: () => [
@@ -16,23 +16,28 @@ export const Route = createFileRoute("/gallery")({
   ],
   component: () => {
     const [slideshow, setSlideshow] = useState<string>("Fall 2024");
-    const [slideshowLink, setSlideshowLink] = useState<string>("Fall 2024");
+    const [slideshowLink, setSlideshowLink] = useState<string>("");
 
-    const handleButton = () => {
-      if (slideshow == "Fall 2024") {
-        setSlideshowLink(
-          "https://docs.google.com/document/d/1gjG2aHkh-IYXLQ5vTfmd6uphP7AYYN0M4liorjPt77k/edit?tab=t.0",
-        );
-      } else if (slideshow == "Spring 2024") {
-        setSlideshowLink(
-          "https://docs.google.com/document/d/1SohQfPM2D8fQhf4vkeWPfC9xTE3my4XTfVA-BNYYA9s/edit?tab=t.0",
-        );
-      } else {
-        setSlideshowLink(
-          "https://docs.google.com/document/d/18brpCElaHqD-rFcKd2eG4FGfjnBWXSthrI-aNdqoHYk/edit?tab=t.0",
-        );
-      }
-    };
+    // Map of slideshow names to Google Drive links
+    const slideshowLinks = new Map<string, string>([
+      [
+        "Fall 2024",
+        "https://docs.google.com/document/d/1gjG2aHkh-IYXLQ5vTfmd6uphP7AYYN0M4liorjPt77k/edit?tab=t.0",
+      ],
+      [
+        "Spring 2024",
+        "https://docs.google.com/document/d/1SohQfPM2D8fQhf4vkeWPfC9xTE3my4XTfVA-BNYYA9s/edit?tab=t.0",
+      ],
+      [
+        "Fall 2023",
+        "https://docs.google.com/document/d/18brpCElaHqD-rFcKd2eG4FGfjnBWXSthrI-aNdqoHYk/edit?tab=t.0",
+      ],
+    ]);
+
+    // Update slideshowLink whenever slideshow changes
+    useEffect(() => {
+      setSlideshowLink(slideshowLinks.get(slideshow) || "");
+    }, [slideshow, slideshowLinks]);
 
     return (
       <div>
@@ -47,12 +52,7 @@ export const Route = createFileRoute("/gallery")({
         </div>
         <div className="flex items-center justify-center pb-8"></div>
         <div className="flex-1 text-center">
-          <div className="flex-1 text-center">
-            <GalleryDropdown
-              slideshow={slideshow}
-              setSlideshow={setSlideshow}
-            />
-          </div>
+          <GalleryDropdown slideshow={slideshow} setSlideshow={setSlideshow} />
           <div className="flex w-full justify-center">
             <hr className="h-5 w-10/12 border-t-4 border-saseGreen" />
           </div>
@@ -60,24 +60,18 @@ export const Route = createFileRoute("/gallery")({
             <div className="flex-1 pb-5 pt-5 text-center font-oswald text-4xl">
               <p>{slideshow}</p>
             </div>
-            <div></div>
             <div className="flex items-center justify-center pb-20">
               <GalleryZipExtraction slideshow={slideshow} />
             </div>
           </div>
           <div className="flex items-center justify-center pb-10">
             <a href={slideshowLink} target="__blank">
-              <div
-                className="... flex h-10 items-center justify-center rounded-sm border-2 border-gray-700 text-white
-                bg-saseBlue transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-saseGreen hover:text-black"
-                onClick={handleButton}
-              >
-                {" "}
+              <div className="... delay-50 flex h-10 items-center justify-center rounded-sm border-2 border-gray-700 bg-saseBlue text-white transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-saseGreen hover:text-black">
                 <div className="pl-2 font-redhat">
                   Click here to visit the Google Drives!
                 </div>
                 <div className="pl-2 pr-2">
-                  <IoMdLink size={15} />{" "}
+                  <IoMdLink size={15} />
                 </div>
               </div>
             </a>
