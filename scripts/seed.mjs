cd4321629c22dc8c6c85244111021529259dafba
ssihala <ssihala@tutanota.com>
2024-12-05 17:19:43 -0500

    Add admin user to seed

--- scripts/seed.mjs
+++ scripts/seed.mjs
@@ -1,9 +1,24 @@
 import { createClient } from "@libsql/client";
 import "dotenv/config";
+import bcrypt from "bcryptjs";
+import { generateIdFromEntropySize } from "lucia";
+
+const { genSalt, hash } = bcrypt;
 
 const client = createClient({
   url: "file:local.db",
 });
+
+//CREATES AN ADMIN USER: username:admin, password:Admin@123
+const passSalt = await genSalt(10);
+const adminHash = await hash("Admin@123", passSalt);
+const adminID = generateIdFromEntropySize(16);
+
+client.execute(
+  `INSERT INTO user (id, username, password_hash, time_added, time_updated, points, roles) 
+    VALUES (?, ?, ?, ?, ?, ?, ?)`,
+  [adminID, "admin", adminHash, Date.now(), Date.now(), 0, "admin"],
+);
 
 (async () => {
   try {
@@ -11,15 +26,10 @@ const client = createClient({
     // const tables = await client.execute(`SELECT name FROM sqlite_master WHERE type='table';`);
     // console.log("Existing tables:", tables.rows);
 
-    // Insert into the 'user' table
-    await client.execute(
-      "INSERT INTO user (id, username, password_hash, time_added, time_updated, points, roles) VALUES ('0000', 'test_username', 'test_hash', 0, 0, 0, 'admin')",
-    );
-
     // Insert into the 'blog' table
     await client.execute(
       `INSERT INTO blog (id, title, content, author_id, published_date, time_updated, tags) 
-       VALUES (?, ?, ?, ?, ?, ?, ?)`,
+     VALUES (?, ?, ?, ?, ?, ?, ?)`,
       [
         "TEST_ID", // Blog ID
         "My First Blog", // Title
