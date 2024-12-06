169ee34f465576d4767c961b250d590cc30dccc9
ssihala <ssihala@tutanota.com>
2024-12-05 20:13:51 -0500

    Add username retrieval from session

--- src/server/api/profile.ts
+++ src/server/api/profile.ts
@@ -8,9 +8,22 @@ import { generateIdFromEntropySize } from "lucia";
 const profileRoutes = new Hono();
 
 profileRoutes.get("/profile/", async (c) => {
-  const body = await c.req.header("Cookie");
-  console.log(body);
-  return new Response("Success");
+  const cookie = await c.req.header("Cookie");
+  let sessionID = "";
+  if (cookie && cookie.startsWith("sessionId=")) {
+    sessionID = cookie.substring("sessionId=".length); // Extract the value after "sessionID="
+  }
+  const result = await db
+    .select({ username: Schema.users.username })
+    .from(Schema.users)
+    .innerJoin(Schema.sessions, eq(Schema.users.id, Schema.sessions.user_id))
+    .where(eq(Schema.sessions.id, sessionID));
+
+  if (result.length == 1) {
+    return c.json(result[0]);
+  } else {
+    console.error("More than one user for a session");
+  }
 });
 
 export default profileRoutes;
