541e18c8bb079303f7099d31355f353bf33ab000
ssihala <ssihala@tutanota.com>
2024-12-05 21:00:54 -0500

    Fix Profile endpoint name

--- src/server/api/profile.ts
+++ src/server/api/profile.ts
@@ -1,28 +1,28 @@
 import { db } from "@db/index";
 import * as Schema from "@db/tables";
-import bcrypt from "bcryptjs";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
-import { generateIdFromEntropySize } from "lucia";
 
 const profileRoutes = new Hono();
 
-profileRoutes.get("/profile/", async (c) => {
-  const cookie = await c.req.header("Cookie");
+profileRoutes.get("/profile", async (c) => {
+  const cookie = c.req.header("Cookie");
+  console.log(cookie);
   let sessionID = "";
   if (cookie && cookie.startsWith("sessionId=")) {
-    sessionID = cookie.substring("sessionId=".length); // Extract the value after "sessionID="
+    sessionID = cookie.substring("sessionId=".length); // Extract the value after "sessionId="
   }
   const result = await db
     .select({ username: Schema.users.username })
     .from(Schema.users)
     .innerJoin(Schema.sessions, eq(Schema.users.id, Schema.sessions.user_id))
     .where(eq(Schema.sessions.id, sessionID));
-
+  console.log(result);
   if (result.length == 1) {
     return c.json(result[0]);
   } else {
     console.error("More than one user for a session");
+    return c.error;
   }
 });
 
