d55aa904b23c2afa8637cf1d680bfba2c6d15441
Ricky Zhang <rickyzhang196@outlook.com>
2024-12-05 20:19:47 -0500

    temp slide sizing fix

--- src/client/components/events/ImageButton.tsx
+++ src/client/components/events/ImageButton.tsx
@@ -18,7 +18,11 @@ const ImageButton: React.FC<ImageButtonProps> = ({ imageUrl, slideUrl }) => {
       <img src={imageUrl} onClick={openModal} className="h-[180px] w-[320px] cursor-pointer hover:opacity-90 focus:outline-none" />
       {isModalOpen && (
         <Modal isOpen={isModalOpen} onClose={closeModal} title="Image Preview" description="This is a preview of the selected image.">
-          <iframe src={slideUrl} width="960" height="569"></iframe>
+          <div className="relative min-h-[290px] w-full min-w-[450px] pb-[56.25%]">
+            {" "}
+            {/* Aspect ratio 16:9 */}
+            <iframe src={slideUrl} className="absolute left-0 top-0 h-full w-full" allowFullScreen></iframe>
+          </div>
         </Modal>
       )}
     </div>
