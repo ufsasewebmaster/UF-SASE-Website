b30a4e52c09dcf411e8b0347f3d88e2991e4f260
Ricky Zhang <rickyzhang196@outlook.com>
2025-01-30 02:12:09 -0500

    wtf why did bun breaking changes env vars

--- src/server/env.ts
+++ src/server/env.ts
@@ -1,6 +1,10 @@
 import { createEnv } from "@t3-oss/env-core";
+import dotenv from "dotenv";
 import { z } from "zod";
-import "dotenv/config";
+
+dotenv.config();
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+console.log("RESEND_API_KEY:", process.env.RESEND_API_KEY);
 
 // This parses the enviorment variables according to a zod schema and provides them to the server
 export const SERVER_ENV = createEnv({
