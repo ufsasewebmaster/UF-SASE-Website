32fd9bcc4e8ec8820e5640771a16ae58604c04a8
Ricky Zhang <rickyzhang196@outlook.com>
2025-01-30 02:27:10 -0500

    Remove console logs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,6 @@
 import { SERVER_ENV } from "@server/env";
 import { defineConfig } from "drizzle-kit";
 
-// Log the database credentials
-console.log("DATABASE_URL:", SERVER_ENV.DATABASE_URL);
-console.log("DATABASE_AUTH_TOKEN:", SERVER_ENV.DATABASE_AUTH_TOKEN);
-
 export default defineConfig({
   schema: "./src/server/db/tables.ts",
   dialect: "turso",
