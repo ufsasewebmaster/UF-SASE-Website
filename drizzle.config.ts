7520691b67b3b39f13fa7b9d1f49bcd34187d4b4
Ricky Zhang <rickyzhang196@outlook.com>
2025-01-31 10:31:26 -0500

    adjust drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,14 @@
-import { SERVER_ENV } from "@server/env";
 import { defineConfig } from "drizzle-kit";
+import { env } from "src/env";
 
 export default defineConfig({
   schema: "./src/server/db/tables.ts",
-  dialect: "turso",
+  dialect: "sqlite",
+  driver: "turso",
   out: "./drizzle",
   dbCredentials: {
-    url: SERVER_ENV.DATABASE_URL,
-    authToken: SERVER_ENV.DATABASE_AUTH_TOKEN,
+    url: env.DATABASE_URL,
+    authToken: env.DATABASE_AUTH_TOKEN,
   },
   verbose: true,
   strict: true,
