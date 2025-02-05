c066189ec8c0159cf6f32fec316274a8437f8c94
zouhelen <helenzou05@gmail.com>
2025-02-05 17:58:44 -0500

    dark mode sports page

--- src/client/components/programs/InfoCard.tsx
+++ src/client/components/programs/InfoCard.tsx
@@ -8,10 +8,10 @@ const InfoCard: React.FC<SimpleCardProps> = ({ text }) => {
   return (
     <div className="relative mx-auto mb-14 w-full max-w-4xl p-5">
       {/* Main Card with Black Border and Green Shadow */}
-      <div className="relative flex flex-col items-center rounded-3xl border-2 border-black bg-gray-100 p-12 shadow-[24px_24px_0px_#7DC242]">
+      <div className="relative flex flex-col items-center rounded-3xl border-2 border-border bg-muted p-12 shadow-[24px_24px_0px_#7DC242]">
         {/* Text Content */}
         <div className="flex flex-col justify-between">
-          <p className="mb-4 text-xl text-black">{text}</p>
+          <p className="mb-4 text-xl text-foreground">{text}</p>
         </div>
       </div>
     </div>
--- src/client/components/programs/TestimonialCard.tsx
+++ src/client/components/programs/TestimonialCard.tsx
@@ -10,9 +10,9 @@ interface TestimonialCardProps {
 
 const TestimonialCard: React.FC<TestimonialCardProps> = ({ image, name, text, title }) => {
   return (
-    <div className="mb-24 flex max-w-7xl flex-col items-center gap-12 bg-white p-6 md:flex-row">
+    <div className="mb-24 flex max-w-7xl flex-col items-center gap-12 bg-background p-6 md:flex-row">
       {/* Image Section */}
-      <div className="relative flex-shrink-0 rounded-3xl border-2 border-black shadow-[18px_18px_0px_#7DC242]">
+      <div className="relative flex-shrink-0 rounded-3xl border-2 border-border shadow-[18px_18px_0px_#7DC242]">
         <img src={image} alt={name} className="h-[500px] w-[400px] rounded-3xl object-cover" />
         {/* Decorative Icon */}
         <img src={imageUrls["Star.png"]} alt="Decorative star" className="absolute -bottom-6 -left-9 h-20 w-20" />
@@ -20,7 +20,7 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ image, name, text, ti
 
       {/* Text Section */}
       <div className="border-l-4 border-saseGreen pl-6">
-        <p className="mb-4 text-3xl text-gray-800">“{text}”</p>
+        <p className="mb-4 text-3xl text-muted-foreground">“{text}”</p>
         <p className="font-semibold text-saseGreen">
           {name}, {title}
         </p>
