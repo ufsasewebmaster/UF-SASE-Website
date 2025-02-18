1914fb95a47ca427ebbdfef6959ea69be40c4a0c
ssihala <ssihala@tutanota.com>
2025-02-18 18:51:05 -0500

    Add route to update profile information

--- src/server/api/profile.ts
+++ src/server/api/profile.ts
@@ -1,6 +1,6 @@
 import { db } from "@/server/db/db";
 import * as Schema from "@db/tables";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns } from "drizzle-orm";
 import { Hono } from "hono";
 
 const profileRoutes = new Hono();
@@ -52,4 +52,58 @@ profileRoutes.get("/profile", async (c) => {
   }
 });
 
+profileRoutes.patch("/profile", async (c) => {
+  const cookie = c.req.header("Cookie") || "";
+  const sessionIDMatch = cookie.match(/sessionId=([^;]*)/);
+  if (!sessionIDMatch) {
+    return c.json({ error: { code: 400, message: "Missing or invalid session ID" } }, 400);
+  }
+  const sessionID = sessionIDMatch[1];
+
+  const result = await db.select().from(Schema.sessions).where(eq(Schema.sessions.id, sessionID));
+  if (result.length > 0) {
+    //Creates a set of column names for personal and professional info respectively
+    const columnNames = generateColumns();
+    const personalColumns = columnNames[0];
+    const professionalColumns = columnNames[1];
+    const userID = result[0].user_id;
+
+    const body = await c.req.json();
+
+    const updatePromises = Object.keys(body).map(async (key) => {
+      if (key in profileSchema) {
+        const value = body[key];
+
+        //Update based on the column name and its respective table
+        if (personalColumns.has(key)) {
+          await db
+            .update(Schema.personalInfo)
+            .set({ [key]: value })
+            .where(eq(Schema.personalInfo.user_id, userID));
+        } else if (professionalColumns.has(key)) {
+          await db
+            .update(Schema.professionalInfo)
+            .set({ [key]: value })
+            .where(eq(Schema.professionalInfo.user_id, userID));
+        }
+      }
+    });
+
+    try {
+      await Promise.all(updatePromises);
+    } catch (err) {
+      console.error("Error:", err);
+      return c.json({ error: { code: 500, message: "Failed to update info" } }, 500);
+    }
+  } else {
+    return c.json({ error: { code: 400, message: "Invalid session" } }, 400);
+  }
+  return c.json({ message: "Profile updated successfully" }, 200);
+});
 export default profileRoutes;
+
+function generateColumns() {
+  const personalInfoColumns = new Set(Object.values(getTableColumns(Schema.personalInfo)).map((col) => col.name));
+  const professionalInfoColumns = new Set(Object.values(getTableColumns(Schema.professionalInfo)).map((col) => col.name));
+  return [personalInfoColumns, professionalInfoColumns];
+}
