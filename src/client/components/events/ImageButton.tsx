0cada4632011cf5765798636491e50f2c01bb32b
Ricky Zhang <rickyzhang196@outlook.com>
2025-02-25 17:20:39 -0500

    Revert "Events slide component implementation (#229)" (#252)
    This reverts commit 1a5b579a6a1b62e041fbfdcfec1c607ee46d75c0.

--- src/client/components/events/ImageButton.tsx
+++ src/client/components/events/ImageButton.tsx
@@ -5,11 +5,9 @@ interface ImageButtonProps {
   imageUrl: string;
   buttonLabel?: string;
   slideUrl: string;
-  imgWidth?: string;
-  imgHeight?: string;
 }
 
-const ImageButton: React.FC<ImageButtonProps> = ({ imageUrl, imgWidth = "320px", slideUrl }) => {
+const ImageButton: React.FC<ImageButtonProps> = ({ imageUrl, slideUrl }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
@@ -17,25 +15,13 @@ const ImageButton: React.FC<ImageButtonProps> = ({ imageUrl, imgWidth = "320px",
 
   return (
     <div>
-      <div
-        className="relative aspect-[16/9] cursor-pointer overflow-hidden rounded-lg hover:opacity-90 focus:outline-none"
-        style={{ width: imgWidth }}
-        onClick={openModal}
-      >
-        <img src={imageUrl} alt="Slide thumbnail" className="absolute left-0 top-0 h-full w-full object-cover" />
-      </div>
-
+      <img src={imageUrl} onClick={openModal} className="h-[180px] w-[320px] cursor-pointer hover:opacity-90 focus:outline-none" />
       {isModalOpen && (
-        <Modal isOpen={isModalOpen} title="" description="" onClose={closeModal}>
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 p-5">
-            <div className="relative flex h-[80vh] max-h-[700px] w-[80vw] max-w-[1200px] items-center justify-center rounded-lg bg-white shadow-lg">
-              <button onClick={closeModal} className="absolute left-5 top-5 z-10 rounded-full bg-black p-2 text-white">
-                X
-              </button>
-              <div className="relative aspect-[16/9] h-full w-full">
-                <iframe src={slideUrl} className="absolute left-0 top-0 h-full w-full rounded-lg" allowFullScreen></iframe>
-              </div>
-            </div>
+        <Modal isOpen={isModalOpen} onClose={closeModal} title="Image Preview" description="This is a preview of the selected image.">
+          <div className="relative min-h-[290px] w-full min-w-[450px] pb-[56.25%]">
+            {" "}
+            {/* Aspect ratio 16:9 */}
+            <iframe src={slideUrl} className="absolute left-0 top-0 h-full w-full" allowFullScreen></iframe>
           </div>
         </Modal>
       )}
