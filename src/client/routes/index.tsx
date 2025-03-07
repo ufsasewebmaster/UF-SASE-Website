c235d3f45e5d5c00ff7da8093b6282b6826923b5
raspberrysir <141175376+raspberrysir@users.noreply.github.com>
2025-03-07 11:51:19 -0500

    Added read more feature

--- src/client/routes/index.tsx
+++ src/client/routes/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import MemberCard from "@/client/components/home/MemberCard";
 import MissionCard from "@/client/components/home/MissionCard";
 import SponsorInfo from "@/client/components/sponsors/SponsorInfo";
@@ -5,13 +6,21 @@ import BoardPic from "@assets/home/Board.png";
 import { imageUrls } from "@assets/imageUrls";
 import Carousel from "@components/carousel/Carousel";
 import SponsorCard from "@components/sponsors/SponsorCard";
-import useIsMobile from "@hooks/useIsMobile";
 import { createFileRoute } from "@tanstack/react-router";
-import MobileMemberCard from "../components/home/MobileMemberCard";
 
 export const Route = createFileRoute("/")({
   component: () => {
-    const isMobile = useIsMobile();
+    const [readMore, setReadMore] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth < 1024);
+      };
+      handleResize();
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
     return (
       <div className="flex flex-col items-center">
@@ -52,21 +61,33 @@ export const Route = createFileRoute("/")({
                   allowFullScreen
                 ></iframe>
                 <p className="font-redhat text-lg text-white md:text-xl">
-                  The <span className="font-semibold">Society of Asian Scientists & Engineers </span> is a vibrant and dynamic organization at the
-                  University of Florida. We are committed to fostering meaningful connections across cultures and empowering{" "}
-                  <span className="font-semibold">Asian Pacific Islander Desi American (APIDA) </span>
-                  professionals in <span className="font-semibold">science and engineering</span>.
+                  The <span className="font-semibold">Society of Asian Scientists & Engineers</span> is a vibrant and dynamic organization at the University of Florida. We are committed to fostering meaningful connections across cultures and empowering <span className="font-semibold">Asian Pacific Islander Desi American (APIDA)</span> professionals in <span className="font-semibold">science and engineering</span>.
                 </p>
-                <br />
-                <p className="font-redhat text-lg text-white md:text-xl">
-                  Through <span className="font-semibold">engaging meetings and events</span>, we provide a nurturing environment where you can
-                  acquire <span className="font-semibold">essential skills and knowledge </span>
-                  to excel in the professional world. Our <span className="font-semibold">inclusive community </span>
-                  welcomes individuals from all majors, offering a friendly atmosphere to help you secure internships, jobs, and network with
-                  like-minded peers. Beyond <span className="font-semibold">personal growth</span>, we are dedicated to making a positive impact in
-                  our <span className="font-semibold">local communities</span>. By celebrating diversity and embracing our heritage, we create{" "}
-                  <span className="font-semibold">opportunities </span>for our members to contribute meaningfully to society.
-                </p>
+                {isMobile && !readMore ? (
+                  <>
+                    <p className="font-redhat text-lg text-white md:text-xl">Through engaging meetings and events, we provide a nurturing environment where you can acquire essential skills...</p>
+                    <button
+                      onClick={() => setReadMore(true)}
+                      className="mt-2 text-saseGreen underline"
+                    >
+                      Read More
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <p className="font-redhat text-lg text-white md:text-xl">
+                      Through <span className="font-semibold">engaging meetings and events</span>, we provide a nurturing environment where you can acquire <span className="font-semibold">essential skills and knowledge</span> to excel in the professional world. Our <span className="font-semibold">inclusive community</span> welcomes individuals from all majors, offering a friendly atmosphere to help you secure internships, jobs, and network with like-minded peers. Beyond <span className="font-semibold">personal growth</span>, we are dedicated to making a positive impact in our <span className="font-semibold">local communities</span>. By celebrating diversity and embracing our heritage, we create <span className="font-semibold">opportunities</span> for our members to contribute meaningfully to society.
+                    </p>
+                    {isMobile && (
+                      <button
+                        onClick={() => setReadMore(false)}
+                        className="mt-2 text-saseGreen underline"
+                      >
+                        Read Less
+                      </button>
+                    )}
+                  </>
+                )}
               </div>
               <img
                 src={imageUrls["SASELogoStar.png"]}
@@ -77,7 +98,7 @@ export const Route = createFileRoute("/")({
           </div>
         </div>
 
-        <div className="flex w-full flex-col items-center bg-saseGray p-12">
+        <div className="flex flex-col items-center bg-saseGray p-12">
           <h1 className="pb-12 text-center font-oswald text-6xl font-medium">Our Mission</h1>
           <div className="grid gap-12 pl-4 pr-4 sm:grid-cols-1 md:grid-cols-3">
             <MissionCard
@@ -104,88 +125,63 @@ export const Route = createFileRoute("/")({
           </div>
         </div>
 
-        {isMobile ? (
-          <>
-            <div className="relative grid h-2 w-full grid-cols-2">
-              <div className="w-full bg-gradient-to-r from-transparent via-[#7DC242] to-[#42957B]" />
-              <div className="w-full bg-gradient-to-r from-[#42957B] via-[#0668B3] to-transparent" />
-            </div>
-            <div className="my-2 flex w-full flex-col items-center bg-white px-12">
-              <MobileMemberCard
-                image={imageUrls["President.jpeg"]}
-                name="Vincent Lin"
-                role="President"
-                textColor="blue"
-                quote="Love the SASE Community :)"
-                imageSide="left"
-              />
-              <div className="h-1 w-5/6 bg-gradient-to-r from-saseGreen to-saseBlue" />
-              <MobileMemberCard
-                image={imageUrls["InternalVicePresident.jpeg"]}
-                name="Bryan Park"
-                role="Internal Vice President"
-                textColor="green"
-                quote="I love SASE <3"
-                imageSide="right"
-              />
-              <div className="h-1 w-5/6 bg-gradient-to-l from-saseGreen to-saseBlue" />
-              <MobileMemberCard
-                image={imageUrls["ExternalVicePresident.jpeg"]}
-                name="Kayleen Diaz"
-                role="External Vice President"
-                textColor="blue"
-                quote="Grow professionally with SASE! :D"
-                imageSide="left"
-              />
-            </div>
-            <div className="relative mx-12 grid h-2 w-full grid-cols-2">
-              <div className="w-full bg-gradient-to-r from-transparent via-[#7DC242] to-[#42957B]" />
-              <div className="w-full bg-gradient-to-r from-[#42957B] via-[#0668B3] to-transparent" />
-            </div>
-          </>
-        ) : (
-          <div className="grid grid-cols-3 gap-12 bg-white p-12">
-            <MemberCard image={imageUrls["President.jpeg"]} name="Vincent Lin" role="President" textColor="blue" quote="Love the SASE Community :)" />
-            <MemberCard
-              image={imageUrls["InternalVicePresident.jpeg"]}
-              name="Bryan Park"
-              role="Internal Vice President"
-              textColor="green"
-              quote="I love SASE <3"
-            />
-            <MemberCard
-              image={imageUrls["ExternalVicePresident.jpeg"]}
-              name="Kayleen Diaz"
-              role="External Vice President"
-              textColor="blue"
-              quote="Grow professionally with SASE! :D"
-            />
-          </div>
-        )}
+        <div className="grid grid-cols-1 gap-20 bg-white p-12 lg:grid-cols-3">
+          <MemberCard image={imageUrls["President.jpeg"]} name="Vincent Lin" role="President" textColor="blue" quote="Love the SASE Community :)" />
+          <MemberCard
+            image={imageUrls["InternalVicePresident.jpeg"]}
+            name="Bryan Park"
+            role="Internal Vice President"
+            textColor="green"
+            quote="I love SASE <3"
+          />
+          <MemberCard
+            image={imageUrls["ExternalVicePresident.jpeg"]}
+            name="Kayleen Diaz"
+            role="External Vice President"
+            textColor="blue"
+            quote="Grow professionally with SASE! :D"
+          />
+        </div>
 
         <div className="w-full bg-black p-10">
           <h1 className="w-full pb-12 text-center font-oswald text-6xl text-white">Our Values</h1>
           <Carousel prog="N/A" purpose="Values" />
+          {/* <Carousel /> */}
         </div>
 
-        <div className="mr-2 flex w-full flex-col items-center bg-saseGray p-2">
-          <h1 className="w-full pb-10 text-center font-oswald text-6xl font-medium">Sponsors</h1>
-          <div className="justify-left mb-20 flex w-9/12 flex-wrap items-start gap-14">
-            <div className="ml-1 flex w-full flex-col items-center rounded-2xl border-4 border-black bg-white p-10 shadow-[12px_12px_0px_#7DC242] sm:w-[45%]">
-              <p className="p-4 text-left font-redhat text-xl sm:text-2xl">
-                Are you interested in becoming a partner with the UF Society of Asian Scientists and Engineers (SASE) Chapter?
+        <div className="flex w-full flex-col items-center bg-saseGray p-12">
+          <h1 className="w-full pb-12 text-center font-oswald text-6xl font-medium">Sponsors</h1>
+          <div className="flex w-9/12 flex-col items-center gap-12">
+            <div className="flex w-full flex-col items-center rounded-2xl border-4 border-black bg-white p-10 shadow-[12px_12px_0px_#7DC242]">
+              <p className="p-4 font-redhat text-xl sm:text-2xl">
+                Are you interested in becoming a partner with the{" "}
+                <span className="font-semibold">UF Society of Asian Scientists and Engineers (SASE) Chapter</span>
+                ?
                 <br />
                 <br />
-                To get access to our sponsorship packet, please contact our External Vice President at
+                To get access to our <span className="font-semibold">sponsorship packet </span>
+                please contact our External Vice President, Kayleen Diaz, at
+              </p>
+              <div className="flex w-full items-center">
                 <a href="mailto:ufsase.evp@gmail.com" className="text-saseGreen underline">
-                  {" "}
                   ufsase.evp@gmail.com
                 </a>
-                .
-              </p>
+                <a
+                  href="/sponsors"
+                  className="ml-auto hidden rounded-lg bg-saseGreen px-4 py-2 font-bold text-white transition duration-300 hover:bg-[#486f2b] sm:block"
+                >
+                  Sponsors
+                </a>
+              </div>
+              <div className="mt-4 sm:hidden">
+                <a href="/sponsors" className="rounded-lg bg-saseGreen px-4 py-2 font-bold text-white transition duration-300 hover:bg-[#486f2b]">
+                  Sponsors
+                </a>
+              </div>
             </div>
-            <div className="flex w-full flex-col items-center gap-12 sm:w-[45%]">
-              <div className="grid w-full grid-cols-1 gap-14 sm:grid-cols-2">
+
+            <div className="flex w-full flex-col items-center gap-16">
+              <div className="grid w-7/12 grid-cols-1 gap-20 sm:grid-cols-2">
                 {SponsorInfo.map((sponsor) =>
                   sponsor.tier === "Diamond" ? (
                     <SponsorCard
@@ -199,7 +195,7 @@ export const Route = createFileRoute("/")({
                   ) : null,
                 )}
               </div>
-              <p className="m-0 mt-[-30px] w-full text-center font-redhat text-2xl italic text-black xl:text-3xl">Current Featured Sponsors</p>
+              <p className="m-0 w-full text-center font-redhat text-2xl italic text-black xl:text-3xl">Current Featured Sponsors</p>
             </div>
           </div>
         </div>
