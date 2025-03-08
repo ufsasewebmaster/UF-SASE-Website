91ef34a3a31eec296fed0978a29875268d19e10f
Elliot Liu <elliot.liu21@gmail.com>
2025-03-08 03:41:15 -0500

    removed security vulnerabilities and boilerplate

--- src/server/db/slidescraper.ts
+++ src/server/db/slidescraper.ts
@@ -1,30 +1,16 @@
-//const key = "AIzaSyAbQwIEo6qgXEx9NTNqAgFD94u2IXRozPI";
 //had to get credentials from creating a service account
-//tutorial link: https://giminiani.com/posts/google-drive-api-with-node-js/#create-a-service-account
+import { writeFileSync } from "fs";
+//import { db } from "@/server/db/db";
+//import * as Schema from "@/server/db/tables";
 import { google } from "googleapis";
 
-// If modifying these scopes, delete token.json.
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-
-/**
- * Lists the names and IDs of up to 10 files.
- */
-/*
-const auth = new google.auth.GoogleAuth({
-  keyFile: './credentials.json',
-  scopes: ['https://www.googleapis.com/auth/drive.metadata.readonly'],
-});
-*/
-
+const embedRules = "embed?start=false&loop=false&delayms=3000";
 const auth = new google.auth.GoogleAuth({
   keyFile: "./credentials.json",
   scopes: ["https://www.googleapis.com/auth/drive"],
 });
 //TODO: figure out how to process NextPageToken
 //first get all folder IDs
-//embed?start=false&loop=false&delayms=3000
 const folderMap = new Map();
 const drive = google.drive({ version: "v3", auth });
 
@@ -33,27 +19,58 @@ const drive = google.drive({ version: "v3", auth });
     q: "mimeType='application/vnd.google-apps.folder' and trashed=false",
     fields: "*",
   });
-  driveResponse.data.files?.forEach((folder) => {
-    folderMap.set(folder.id, folder.name);
-  });
-  console.log(folderMap);
-})()
-  .catch((e) => {
-    console.log(e);
-  })
-  .then();
+  if (driveResponse.data.files) {
+    driveResponse.data.files.forEach((folder) => {
+      folderMap.set(folder.id, folder.name);
+    });
+    console.log(folderMap);
+  } else {
+    //if no response, write log into file and abort operation
+    const event = new Date().toUTCString();
+    writeFileSync("errors.log", `Folder retrieval failed at ${event}`, {
+      flag: "w",
+    });
+    return;
+  }
+})().then();
 {
   //retrieve all files
   const allFiles = await drive.files.list({
-    q: "trashed=false",
+    q: "mimeType!='application/vnd.google-apps.folder' and trashed=false",
     fields: "*",
   });
-  allFiles.data.files?.forEach((file) => {
-    if (String(file.mimeType) != "application/vnd.google-apps.folder") {
-      console.log("nonpresentation file: ", file.name, " ", file.id, " ", file.thumbnailLink, " ", file.webViewLink);
-    }
-  });
-  console.log(allFiles.data);
+  if (allFiles.data.files) {
+    allFiles.data.files.forEach((file) => {
+      if (String(file.mimeType) != "application/vnd.google-apps.folder") {
+        //add all nonfolder files to database
+        //console.log("nonpresentation file: ", file.name, " ", file.id, " ", file.thumbnailLink, "/n", file.webViewLink, " ", file.modifiedTime, " ");
+      }
+      //build embed URL
+      if (file.webViewLink) {
+        const editIndex = file.webViewLink.indexOf("/edit");
+        const embed_url = file.webViewLink.substring(0, editIndex + 1) + embedRules;
+        console.log(embed_url);
+      } else {
+        const event = new Date().toUTCString();
+        writeFileSync("errors.log", `File link not present at ${event}`);
+      }
+      //download thumbnail
+      try {
+        const resp = fetch(String(file.thumbnailLink), {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        console.log(resp);
+      } catch {
+        return;
+      }
+    });
+  } else {
+    const event = new Date().toUTCString();
+    writeFileSync("errors.log", `File retrieval failed at ${event}`);
+  }
 }
 
 /*
