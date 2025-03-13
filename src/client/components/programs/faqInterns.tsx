3d9af594b42bc1865be144452c3ebb85539fe211
Stephanie Fong <116681250+stephanietfong@users.noreply.github.com>
2025-03-12 21:31:26 -0400

    fixed faq answers (#285)

---
+++ src/client/components/programs/faqInterns.tsx
@@ -0,0 +1,40 @@
+export const faqData: Array<{ question: string; answer: string | React.ReactNode }> = [
+  {
+    question: "How do I sign up?",
+    answer: (
+      <>
+        <strong>Spring 2025</strong> applications will open in early December and close in early January! Keep an eye out on UF SASE’s{" "}
+        <a href="https://www.instagram.com/ufsase/" target="_blank" rel="noopener noreferrer" className="text-saseGreen underline">
+          Instagram
+        </a>{" "}
+        and{" "}
+        <a href="http://discord.gg/q3HBeC5" target="_blank" rel="noopener noreferrer" className="text-saseGreen underline">
+          Discord
+        </a>{" "}
+        for more details.
+      </>
+    ),
+  },
+  {
+    question: "What do interns do?",
+    answer:
+      "Interns work together in committees to plan events for the whole SASE body, but also get shadowing opportunities, insight into board activities, and more fun!",
+  },
+  {
+    question: "What is the time commitment?",
+    answer: "There are weekly meetings of an hour, but work outside of meetings can vary from 1-3 hours a week.",
+  },
+  {
+    question: "I have more questions!",
+    answer: (
+      <>
+        Feel free to ask in the{" "}
+        <a href="http://discord.gg/q3HBeC5" target="_blank" rel="noopener noreferrer" className="text-saseGreen underline">
+          SASE Discord
+        </a>{" "}
+        or contact our Member Involvement Chairs, Anna and Justin, through Discord!
+      </>
+    ),
+  },
+  // Add more FAQ items here
+];
