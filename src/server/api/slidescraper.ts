9c6e56874c858c63c1e1883c30cb83e17bcedee9
Elliot Liu <elliot.liu21@gmail.com>
2025-03-13 03:07:00 -0400

    set up thumbnail download from GDrive API

---
+++ src/server/api/slidescraper.ts
@@ -0,0 +1,98 @@
+//had to get credentials from creating a service account
+import { writeFile, writeFileSync } from "fs";
+import { db } from "@/server/db/db";
+import * as Schema from "@/server/db/tables";
+import { eq } from "drizzle-orm";
+import { google } from "googleapis";
+
+const embedRules = "embed?start=false&loop=false&delayms=3000";
+const auth = new google.auth.GoogleAuth({
+  keyFile: process.env.CREDENTIALS_PATH,
+  scopes: ["https://www.googleapis.com/auth/drive"],
+});
+//TODO: figure out how to process NextPageToken
+/*Note: by enforcing slide deck naming guidelines for GBMs, can sort w/o relying on timestamps
+said guidelines: all names MUST start with GBM followed by number and underscore
+after that, all hyphens and underscores are replaced by spaces, while spaces and capitalization are left alone
+*/
+
+const folderMap = new Map();
+const drive = google.drive({ version: "v3", auth });
+
+(async () => {
+  const driveResponse = await drive.files.list({
+    q: "mimeType='application/vnd.google-apps.folder' and trashed=false",
+    fields: "*",
+  });
+  if (driveResponse.data.files) {
+    driveResponse.data.files.forEach((folder) => {
+      folderMap.set(folder.id, folder.name);
+    });
+    console.log(folderMap);
+  } else {
+    //if no response, write log into file and abort operation
+    const event = new Date().toUTCString();
+    writeFileSync("errors.log", `Folder retrieval failed at ${event}`, {
+      flag: "w",
+    });
+    return;
+  }
+})().then();
+{
+  //retrieve all files
+  const allFiles = await drive.files.list({
+    q: "mimeType!='application/vnd.google-apps.folder' and trashed=false",
+    fields: "*",
+  });
+  if (allFiles.data.files) {
+    allFiles.data.files.forEach(async (file) => {
+      //skip files that are already in database by searching for them by name
+      const result = await db
+        .select()
+        .from(Schema.meetingSlides)
+        .where(eq(Schema.meetingSlides.name, String(file.name)))
+        .limit(1);
+      if (result.length == 0) {
+        //add all nonfolder files to database
+        console.log("nonpresentation file: ", file.name, " ", file.id, " ", file.thumbnailLink, "/n", file.webViewLink, " ", file.modifiedTime, " ");
+        //build embed URL
+        if (file.webViewLink) {
+          const editIndex = file.webViewLink.indexOf("/edit");
+          const embed_url = file.webViewLink.substring(0, editIndex + 1) + embedRules;
+          console.log(embed_url);
+        } else {
+          const event = new Date().toUTCString();
+          writeFileSync("errors.log", `File link not present at ${event}`);
+        }
+        //download thumbnail
+        try {
+          //change URL to get max image size
+          let linkString: string = String(file.thumbnailLink);
+          linkString = linkString.replace("=s220", "=s880");
+          const resp = await fetch(linkString, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          const blob: Blob = await resp.blob();
+          const arrayBuf: ArrayBuffer = await blob.arrayBuffer();
+          if (typeof arrayBuf !== "string") {
+            const buffer: Buffer = Buffer.from(arrayBuf);
+            writeFile(`images/${file.name}_thumbnail.png`, buffer, { flag: "w" }, (err) => {
+              console.log(err);
+            });
+          }
+          console.log(resp);
+        } catch {
+          writeFileSync("errors.log", `Failed to download thumbnail from link ${file.thumbnailLink}`);
+          return;
+        }
+        //upload record to database
+      }
+    });
+  } else {
+    const event = new Date().toUTCString();
+    writeFileSync("errors.log", `File retrieval failed at ${event}`);
+  }
+}
