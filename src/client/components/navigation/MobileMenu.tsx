8c997c26eaf7da613be1484ac52a173e972fa865
zouhelen <helenzou05@gmail.com>
2025-03-14 17:58:35 -0400

    bun lint fix

--- src/client/components/navigation/MobileMenu.tsx
+++ src/client/components/navigation/MobileMenu.tsx
@@ -2,9 +2,7 @@ import { cn } from "@/shared/utils";
 import { Icon } from "@iconify/react";
 import { UserButton } from "@navigation/UserButton";
 import { useRouter } from "@tanstack/react-router";
-import React, { useContext, useEffect, useState } from "react";
-
-//import { DarkModeContext } from "../DarkModeProvider";
+import React, { useEffect, useState } from "react";
 
 interface NavItem {
   name: string;
@@ -13,13 +11,13 @@ interface NavItem {
 }
 
 interface MobileMenuProps {
-  navItems: Array<NavItem>;
-  isOpen: boolean;
-  onClose: () => void;
+  darkMode: boolean;
   isHomePage: boolean;
   isLoggedIn: boolean;
+  isOpen: boolean;
+  navItems: Array<NavItem>;
+  onClose: () => void;
   onLogout: () => void;
-  darkMode: boolean;
 }
 
 export const MobileMenu: React.FC<MobileMenuProps> = ({ darkMode, isHomePage, isLoggedIn, isOpen, navItems, onClose, onLogout }) => {
@@ -50,12 +48,12 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({ darkMode, isHomePage, is
 };
 
 const MobileNavItem: React.FC<{
+  darkMode: boolean;
+  isHomePage: boolean;
   item: NavItem;
   onClose: () => void;
-  isHomePage: boolean;
   closeAll: boolean;
-  darkMode: boolean;
-}> = ({ closeAll, isHomePage, item, onClose, darkMode }) => {
+}> = ({ closeAll, darkMode, isHomePage, item, onClose }) => {
   const router = useRouter();
   const [submenuOpen, setSubmenuOpen] = useState(false);
 
@@ -86,60 +84,12 @@ const MobileNavItem: React.FC<{
     isHomePage || darkMode ? "text-white hover:text-[#0f6cb6]" : "text-black hover:text-[#0f6cb6]",
   );
 
-  const svgClasses = cn("mr-1 h-4 w-4 transition-transform duration-300");
-
-  const gradientClasses = cn(
-    "mr-4 mt-1 w-auto bg-gradient-to-r transition-all duration-500 ease-in-out",
-    isHomePage || darkMode ? "from-saseGreen via-saseBlue to-black" : "from-saseBlue via-saseGreen to-white",
-    submenuOpen
-      ? "pointer-events-auto max-h-screen translate-y-0 transform opacity-100"
-      : "pointer-events-none max-h-0 -translate-y-2 transform opacity-0",
-  );
-
-  const childUlClasses = cn("flex w-full flex-col items-end space-y-1", isHomePage || darkMode ? "text-white" : "text-black");
-
-  const childButtonClasses = cn("block w-full px-2 py-2 text-right transition-transform duration-300 hover:scale-105");
-
   return (
     <li className={liClasses}>
       <button className={buttonClasses} onClick={handleClick} aria-haspopup="true" aria-expanded={submenuOpen}>
         {item.name === "Home" && <Icon icon="oui:home" className="mr-2 h-4 w-4" />}
-        {item.children && item.children.length > 0 && (
-          <svg
-            className={svgClasses}
-            style={{ transform: submenuOpen ? "rotate(90deg)" : "rotate(0deg)" }}
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
-          </svg>
-        )}
         <span>{item.name}</span>
       </button>
-      {item.children && item.children.length > 0 && (
-        <div className={gradientClasses}>
-          <ul className={childUlClasses}>
-            {item.children.map(
-              (child) =>
-                child.path && (
-                  <li key={child.name} className="w-full">
-                    <button
-                      onClick={() => {
-                        router.navigate({ to: child.path });
-                        onClose();
-                      }}
-                      className={childButtonClasses}
-                    >
-                      {child.name}
-                    </button>
-                  </li>
-                ),
-            )}
-          </ul>
-        </div>
-      )}
     </li>
   );
 };
