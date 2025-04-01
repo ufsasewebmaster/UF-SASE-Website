50e41fe1f42be5855f448cabdb56d793b17ba079
Taebok Joseph Kim <josephkim7104@gmail.com>
2025-04-01 14:15:58 -0400

    prettier pls

--- src/client/components/Modal.tsx
+++ src/client/components/Modal.tsx
@@ -15,8 +15,8 @@ const Modal: React.FC<ModalProps> = ({ category, children, description, isOpen, 
   if (!isOpen) return null;
 
   return (
-    <div className="animate-fade-in fixed inset-0 z-[9999] flex items-center justify-center bg-black/50">
-      <div className="animate-zoom-fade-in relative max-h-[80vh] w-[80vw] max-w-[1200px] overflow-auto rounded-lg bg-white shadow-lg">
+    <div className="fixed inset-0 z-[9999] flex animate-fade-in items-center justify-center bg-black/50">
+      <div className="relative max-h-[80vh] w-[80vw] max-w-[1200px] animate-zoom-fade-in overflow-auto rounded-lg bg-white shadow-lg">
         <div className="flex items-center justify-between border-b px-6 py-3">
           {title && (
             <h2 className="font-redhat text-xl">
