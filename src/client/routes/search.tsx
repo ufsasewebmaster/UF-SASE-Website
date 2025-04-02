626fa8000b9b3bd5119396cd3d6b7e4d1a6b6805
ssihala <ssihala@tutanota.com>
2025-04-02 15:55:01 -0400

    Test fuse

--- src/client/routes/search.tsx
+++ src/client/routes/search.tsx
@@ -1,32 +1,5 @@
-import { createFileRoute, useRouteContext, useRouter } from "@tanstack/react-router";
-
-// import Fuse from "fuse.js";
-// import { router } from "../client";
-
-// const fuseOptions = {
-//   // isCaseSensitive: false,
-//   // includeScore: false,
-//   // ignoreDiacritics: false,
-//   // shouldSort: true,
-//   // includeMatches: false,
-//   // findAllMatches: false,
-//   // minMatchCharLength: 1,
-//   // location: 0,
-//   // threshold: 0.6,
-//   // distance: 100,
-//   // useExtendedSearch: false,
-//   // ignoreLocation: false,
-//   // ignoreFieldNorm: false,
-//   // fieldNormWeight: 1,
-//   keys: ["title", "author.firstName"],
-// };
-
-// const fuse = new Fuse(list, fuseOptions);
-
-// // Change the pattern
-// const searchPattern = "Jeev";
-
-// console.log(fuse.search(searchPattern));
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import Fuse from "fuse.js";
 
 export const Route = createFileRoute("/search")({
   component: () => {
@@ -52,14 +25,33 @@ export const Route = createFileRoute("/search")({
       }
     });
 
+    const fuseOptions = {
+      // isCaseSensitive: false,
+      includeScore: true,
+      // ignoreDiacritics: false,
+      shouldSort: true,
+      // includeMatches: false,
+      // findAllMatches: false,
+      // minMatchCharLength: 1,
+      // location: 0,
+      // threshold: 0.6,
+      distance: 200,
+      useExtendedSearch: true,
+      // ignoreLocation: false,
+      // ignoreFieldNorm: false,
+      // fieldNormWeight: 1,
+      keys: ["title", "description"],
+    };
+
+    const fuse = new Fuse(metaTagsList, fuseOptions);
+
+    // Change the pattern
+    const searchPattern = "Pickleball";
+
     return (
       <div>
         <h1 className="mb-4 text-2xl font-bold">About</h1>
-        {metaTagsList.map((metaTags, index) => (
-          <div key={index}>
-            <pre>{JSON.stringify(metaTags, null, 2)}</pre>
-          </div>
-        ))}
+        <pre>{JSON.stringify(fuse.search(searchPattern), null, 2)}</pre>
       </div>
     );
   },
