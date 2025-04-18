b224282b2d26c8fd53b4c18b43b4941909322751
Ricky Zhang <rickyzhang196@outlook.com>
2025-04-18 07:09:38 -0400

    add dynamic mentor-mentee graph fetching

--- src/client/api/mentorMentee.ts
+++ src/client/api/mentorMentee.ts
@@ -3,6 +3,7 @@ import { MMRelationshipSchema } from "@shared/schema/MMRelationshipSchema";
 import { apiFetch } from "@shared/utils";
 
 export const fetchMentorMenteeRelations = async (): Promise<Array<MMRelationship>> => {
-  const response = await apiFetch("/api/mentorMentee/all", { method: "GET" }, MMRelationshipSchema.array());
-  return response.data;
-};
+    const response = await apiFetch("/api/mentorMentee/all", { method: "GET"}, MMRelationshipSchema.array());
+    return response.data;
+}
+
--- src/client/components/programs/MentorMenteeGraph.tsx
+++ src/client/components/programs/MentorMenteeGraph.tsx
@@ -63,7 +63,6 @@ const MentorMenteeGraph: React.FC = () => {
     setTimeout(() => fg.zoomToFit(0, 100), 100);
   }, [dims, graphData]);
 
-  if (!dims) console.log("AAAAAAAAHHHH");
   return (
     <ClientOnly>
       <div ref={containerRef} style={{ width: "100%", height: "600px", border: "1px solid #ccc" }}>
